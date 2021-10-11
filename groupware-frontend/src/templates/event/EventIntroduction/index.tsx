d2310abb397dcd47a89678c3399098d14a271ca6
adgjmptwgw <adgjmptwgw@gmail.com>
2021-10-11 16:12:54 +0900

    Deleted src/types/event/eventIntroduction/types.ts file

--- groupware-frontend/src/templates/event/EventIntroduction/index.tsx
+++ groupware-frontend/src/templates/event/EventIntroduction/index.tsx
@@ -1,8 +1,17 @@
 import eventPRStyles from '@/styles/layouts/EventPR.module.scss';
 import EventCard from '@/components/EventCard';
-
-import { EachEventData } from 'src/types/event/eventIntroduction/types';
 import Image from 'next/image';
+import { EventSchedule } from 'src/types';
+import { EventTab } from 'src/types/header/tab/types';
+
+export interface EachEventData {
+  events?: EventSchedule[];
+  bottomImages: StaticImageData[];
+  headlineImage: JSX.Element | StaticImageData;
+  heading: EventTab;
+  subHeading: string;
+  content: string;
+}
 
 const EventIntroduction: React.FC<EachEventData> = ({
   events,
