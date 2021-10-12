cf15683afdea6b564ea1e87d3b0959b811777b2a
Kodai Kabasawa <kabaaa1126@gmail.com>
2021-10-12 17:30:13 +0900

    test deployment

--- groupware-frontend/src/utils/url/index.ts
+++ groupware-frontend/src/utils/url/index.ts
@@ -1,10 +1,11 @@
 import axios from 'axios';
 import { jwtJsonHeader } from './header';
 
-const baseURL =
-  process.env.NODE_ENV === 'production'
-    ? 'https://groupware-development-sgzkfl3uyq-an.a.run.app'
-    : 'http://localhost:9000';
+const baseURL = process.env.NEXT_PUBLIC_API_URL
+  ? process.env.NEXT_PUBLIC_API_URL
+  : process.env.NODE_ENV === 'production'
+  ? 'https://groupware-development-sgzkfl3uyq-an.a.run.app'
+  : 'http://localhost:9000';
 
 export const axiosInstance = axios.create({ baseURL });
 axiosInstance.defaults.withCredentials = true;
