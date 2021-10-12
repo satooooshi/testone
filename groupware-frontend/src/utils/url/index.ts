58a91ac9066efc2818da0dd37dd45e2286cd89f2
Kodai Kabasawa <kabaaa1126@gmail.com>
2021-10-12 18:06:30 +0900

    fix environment variable

--- groupware-frontend/src/utils/url/index.ts
+++ groupware-frontend/src/utils/url/index.ts
@@ -1,9 +1,10 @@
 import axios from 'axios';
 import { jwtJsonHeader } from './header';
-console.log(process.env.NEXT_PUBLIC_API_URL);
 
 const baseURL = process.env.NEXT_PUBLIC_API_URL
   ? process.env.NEXT_PUBLIC_API_URL
+  : process.env.API_URL
+  ? process.env.API_URL
   : process.env.NODE_ENV === 'production'
   ? 'https://groupware-development-sgzkfl3uyq-an.a.run.app'
   : 'http://localhost:9000';
