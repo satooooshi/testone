3612d01beca2857f6280814c552d4ff6f6d21dc2
Kodai Kabasawa <kabaaa1126@gmail.com>
2021-10-13 18:40:17 +0900

    fix to migrate correctly in empty database

--- groupware-server/src/migrations/1634108116096-add-column-on-user-joining-event-table.ts
+++ groupware-server/src/migrations/1634108116096-add-column-on-user-joining-event-table.ts
@@ -28,21 +28,9 @@ export class addColumnOnUserJoiningEventTable1634108116096
     await queryRunner.query(
       `ALTER TABLE user_joining_event CHANGE event_id event_id int NULL`,
     );
-    await queryRunner.query(
-      `ALTER TABLE user_joining_event ADD CONSTRAINT FK_fb26c4d1ce414dddb63c9957453 FOREIGN KEY (user_id) REFERENCES users(id) ON DELETE CASCADE ON UPDATE CASCADE`,
-    );
-    await queryRunner.query(
-      `ALTER TABLE user_joining_event ADD CONSTRAINT FK_8e3906a795ab4dd368085070a36 FOREIGN KEY (event_id) REFERENCES events(id) ON DELETE CASCADE ON UPDATE CASCADE`,
-    );
   }
 
   public async down(queryRunner: QueryRunner): Promise<void> {
-    await queryRunner.query(
-      `ALTER TABLE user_joining_event DROP FOREIGN KEY FK_8e3906a795ab4dd368085070a36`,
-    );
-    await queryRunner.query(
-      `ALTER TABLE user_joining_event DROP FOREIGN KEY FK_fb26c4d1ce414dddb63c9957453`,
-    );
     await queryRunner.query(
       `ALTER TABLE user_joining_event CHANGE event_id event_id int NOT NULL`,
     );
