5ba1c3c2a3d85b2622391586e6c0f8792123a242
Kodai Kabasawa <kabaaa1126@gmail.com>
2021-10-14 18:51:17 +0900

    add late minutes selector in EventParticepants component

--- groupware-frontend/src/components/event/EventParticepants/index.tsx
+++ groupware-frontend/src/components/event/EventParticepants/index.tsx
@@ -1,17 +1,40 @@
 import React, { useState } from 'react';
 import eventParticipantsStyles from '@/styles/components/EventParticipants.module.scss';
 import { UserJoiningEvent } from 'src/types';
-import { Avatar } from '@chakra-ui/react';
+import {
+  Avatar,
+  Button,
+  Input,
+  Menu,
+  MenuButton,
+  MenuItem,
+  MenuItemOption,
+  MenuList,
+  MenuOptionGroup,
+} from '@chakra-ui/react';
 import Link from 'next/link';
 
 type EventParticipantsProps = {
   userJoiningEvent: UserJoiningEvent[];
+  // lateMinutes: number;
 };
 
 const EventParticipants: React.FC<EventParticipantsProps> = ({
   userJoiningEvent,
+  // lateMinutes,
 }) => {
   const [allVisible, setAllVisible] = useState(false);
+  const [m, setM] = useState('');
+  const lateMinutesText = (v: number) => {
+    let message = '';
+    const hour = Math.floor(v / 60);
+    const minutes = v % 60;
+    if (hour) {
+      message += `${hour}時間`;
+    }
+    message += `${minutes}分遅刻`;
+    return message;
+  };
   return (
     <div className={eventParticipantsStyles.participants_area}>
       <div className={eventParticipantsStyles.participant_top}>
@@ -28,17 +51,38 @@ const EventParticipants: React.FC<EventParticipantsProps> = ({
       </div>
       {userJoiningEvent.map((u, index) =>
         index <= 15 || allVisible ? (
-          <Link key={u.user.id} href={`/account/${u.user.id}`}>
-            <a className={eventParticipantsStyles.participant_name_wrapper}>
-              <Avatar
-                src={u.user.avatarUrl}
-                className={eventParticipantsStyles.participant_avatar}
-              />
-              <p className={eventParticipantsStyles.participant_name}>
-                {u.user.lastName + ' ' + u.user.firstName}
-              </p>
-            </a>
-          </Link>
+          <div className={eventParticipantsStyles.participant_name_wrapper}>
+            <Link key={u.user.id} href={`/account/${u.user.id}`}>
+              <a>
+                <Avatar
+                  src={u.user.avatarUrl}
+                  className={eventParticipantsStyles.participant_avatar}
+                />
+              </a>
+            </Link>
+            <p className={eventParticipantsStyles.participant_name}>
+              {u.user.lastName + ' ' + u.user.firstName}
+            </p>
+            <Menu>
+              <MenuButton as={Button}>
+                {m ? lateMinutesText(Number(m)) : '遅刻を記録'}
+              </MenuButton>
+              <MenuList>
+                <MenuOptionGroup
+                  onChange={(v) => setM(v)}
+                  defaultValue={''}
+                  type="radio">
+                  <MenuItemOption value="">記録しない</MenuItemOption>
+                  <MenuItemOption value="15">15分遅刻</MenuItemOption>
+                  <MenuItemOption value="30">30分遅刻</MenuItemOption>
+                  <MenuItemOption value="45">45分遅刻</MenuItemOption>
+                  <MenuItemOption value="60">1時間遅刻</MenuItemOption>
+                  <MenuItemOption value="90">1時間半遅刻</MenuItemOption>
+                  <MenuItemOption value="120">2時間以上遅刻</MenuItemOption>
+                </MenuOptionGroup>
+              </MenuList>
+            </Menu>
+          </div>
         ) : (
           <></>
         ),
