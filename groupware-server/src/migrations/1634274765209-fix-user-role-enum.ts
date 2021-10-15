93a4f7aa0282592c3838332e3f78d644064c41c3
Kodai Kabasawa <kabaaa1126@gmail.com>
2021-10-15 14:25:27 +0900

    escape migration error

--- groupware-server/src/migrations/1634274765209-fix-user-role-enum.ts
+++ groupware-server/src/migrations/1634274765209-fix-user-role-enum.ts
@@ -5,11 +5,23 @@ export class fixUserRoleEnum1634274765209 implements MigrationInterface {
 
   public async up(queryRunner: QueryRunner): Promise<void> {
     await queryRunner.query(
+      `ALTER TABLE users CHANGE role role enum ('admin', 'instructor', 'head_office', 'coach', 'common') NOT NULL DEFAULT 'common'`,
+    );
+    await queryRunner.query(
+      `UPDATE users SET role = 'coach' WHERE role = 'head_office'`,
+    );
+    await queryRunner.query(
       `ALTER TABLE users CHANGE role role enum ('admin', 'instructor', 'coach', 'common') NOT NULL DEFAULT 'common'`,
     );
   }
 
   public async down(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.query(
+      `ALTER TABLE users CHANGE role role enum ('admin', 'instructor', 'head_office', 'coach', 'common') NOT NULL DEFAULT 'common'`,
+    );
+    await queryRunner.query(
+      `UPDATE users SET role = 'head_office' WHERE role = 'coach'`,
+    );
     await queryRunner.query(
       `ALTER TABLE users CHANGE role role enum ('admin', 'instructor', 'head_office', 'common') NOT NULL DEFAULT 'common'`,
     );
