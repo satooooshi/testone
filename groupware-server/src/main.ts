b00bd951982f270fcbc0d7254522efb3453f2643
Kodai Kabasawa <kabaaa1126@gmail.com>
2021-10-18 22:58:15 +0900

    add origin for cors

--- groupware-server/src/main.ts
+++ groupware-server/src/main.ts
@@ -32,6 +32,8 @@ async function bootstrap() {
       'https://groupware-frontend-git-develop-valleyin-dev.vercel.app',
       'https://groupware-frontend-theta.vercel.app',
       'https://groupware-seven.vercel.app',
+      'https://groupware-frontend-sgzkfl3uyq-an.a.run.app',
+      '*',
       process.env.CLIENT_DOMAIN,
     ],
   });
