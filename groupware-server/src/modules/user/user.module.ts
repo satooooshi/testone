ed789700d7f2d4ccac98ee1c0d364168699a9c15
Kodai Kabasawa <kabaaa1126@gmail.com>
2021-10-19 17:27:03 +0900

    return avatar url of users as signed url CU-1p1jpgh[completed]

--- groupware-server/src/modules/user/user.module.ts
+++ groupware-server/src/modules/user/user.module.ts
@@ -1,11 +1,12 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { User } from 'src/entities/user.entity';
+import { StorageModule } from '../storage/storage.module';
 import { UserController } from './user.controller';
 import { UserService } from './user.service';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([User])],
+  imports: [StorageModule, TypeOrmModule.forFeature([User])],
   controllers: [UserController],
   providers: [UserService],
   exports: [UserService],
