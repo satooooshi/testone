8f11b945cfaec7e51c283992dc4fdf69d976d3f1
Kodai Kabasawa <kabaaa1126@gmail.com>
2021-10-21 10:18:34 +0900

    fix query param to search users' wiki CU-1p7b31m[completed]

--- groupware-frontend/src/utils/wikiQueryRefresh.ts
+++ groupware-frontend/src/utils/wikiQueryRefresh.ts
@@ -9,9 +9,17 @@ export const wikiQueryRefresh = (
     word: newWord = query.word || '',
     status: newStatus = query.status || 'new',
     type: newType = query.type || '',
+    writer: newWriter = query.writer || '',
+    answer_writer: newAnswerWriter = query.answer_writer || '',
     rule_category: newRuleCategory = query.rule_category || '',
   } = query;
   let searchQuery = `page=${newPage}&tag=${newTag}&word=${newWord}&status=${newStatus}&type=${newType}`;
+  if (newWriter) {
+    searchQuery += `&writer=${newWriter}`;
+  }
+  if (newAnswerWriter) {
+    searchQuery += `&answer_writer=${newAnswerWriter}`;
+  }
   if (newRuleCategory) {
     searchQuery += `&rule_category=${newRuleCategory}`;
   }
