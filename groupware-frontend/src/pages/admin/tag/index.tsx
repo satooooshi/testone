abbecd2d52e466e72f26c3d360ebbc781e03f6a2
Kodai Kabasawa <kabaaa1126@gmail.com>
2021-10-22 19:47:30 +0900

    tiny fix

--- groupware-frontend/src/pages/admin/tag/index.tsx
+++ groupware-frontend/src/pages/admin/tag/index.tsx
@@ -1,9 +1,8 @@
 import { Tab } from 'src/types/header/tab/types';
 import LayoutWithTab from '@/components/layout/LayoutWithTab';
 import { SidebarScreenName } from '@/components/layout/Sidebar';
-import React, { useEffect } from 'react';
+import React from 'react';
 import { Tag, TagType, UserRole, UserTag } from 'src/types';
-import { useRouter } from 'next/router';
 import Head from 'next/head';
 import { useAuthenticate } from 'src/contexts/useAuthenticate';
 import { useHeaderTab } from '@/hooks/headerTab/useHeaderTab';
@@ -15,18 +14,16 @@ import { useAPIDeleteTag } from '@/hooks/api/tag/useAPIDeleteTag';
 import { useToast } from '@chakra-ui/react';
 
 const TagAdmin: React.FC = () => {
-  const router = useRouter();
   const toast = useToast();
   const { user } = useAuthenticate();
   const { data: tags, refetch } = useAPIGetTag();
   const modifyStrToFlat = (targetString: string) => {
     const deleteSymbolFromStr = (str: string) => {
-      return str.replace(/[^a-zA-Z ]/g, '');
+      return str.replace(/[^ぁ-んァ-ンーa-zA-Z0-9一-龠０-９\-\r]/g, '');
     };
     const hiraToKana = (str: string) => {
-      return str.replace(/[\u3041-\u3096]/g, (match) => {
-        const chr = match.charCodeAt(0) + 0x60;
-        return String.fromCharCode(chr);
+      return str.replace(/[ぁ-ん]/g, (s) => {
+        return String.fromCharCode(s.charCodeAt(0) - 0x60);
       });
     };
     const escaped = deleteSymbolFromStr(targetString);
