66f5cde413364a2399f2bca0dc4f1163cfb958bd
kosei-program-dev <kosei.program.dev@gmail.com>
2021-10-28 14:32:22 +0900

    fix arguments of useReducer
    #CU-1pjurdx

--- groupware-frontend/src/pages/chat/[id].tsx
+++ groupware-frontend/src/pages/chat/[id].tsx
@@ -160,7 +160,10 @@ const ChatDetail = () => {
   const { mutate: createGroup } = useAPISaveChatGroup({
     onSuccess: () => {
       dispatchModal({ type: 'createGroupWindow', value: false });
-      dispatchChat({ type: 'newGroup', value: { ...newGroup, members: [] } });
+      dispatchChat({
+        type: 'newGroup',
+        value: { ...newGroup, name: '', members: [] },
+      });
       refetchGroups();
     },
   });
