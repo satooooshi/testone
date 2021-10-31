b7f3f6454207289bbbf93b7df5783c141d90cbee
kosei-program-dev <kosei.program.dev@gmail.com>
2021-10-31 14:32:12 +0900

    tiny fix
    #CU-1q3zfe2

--- groupware-frontend/src/pages/admin/users/new/index.tsx
+++ groupware-frontend/src/pages/admin/users/new/index.tsx
@@ -269,6 +269,7 @@ const CreateNewUser = () => {
                 })
               }
               onImageLoaded={onLoad}
+              circularCrop={true}
             />
           ) : (
             <div
