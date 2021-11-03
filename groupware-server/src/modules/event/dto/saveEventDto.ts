296b528af3eb6590704eac10ef0a1d8a78b7b578
kosei-program-dev <kosei.program.dev@gmail.com>
2021-11-03 17:37:54 +0900

    fix event Dto
    #CU-1q9rk7a

--- groupware-server/src/modules/event/dto/saveEventDto.ts
+++ groupware-server/src/modules/event/dto/saveEventDto.ts
@@ -1,15 +1,23 @@
+import { Type } from 'class-transformer';
 import {
   ArrayNotEmpty,
   IsArray,
+  IsBoolean,
+  IsDate,
+  IsEnum,
   IsNotEmpty,
   IsString,
+  MinDate,
   ValidateIf,
 } from 'class-validator';
+import { EventType } from 'src/entities/event.entity';
 import { isNotEmptyExceptTags } from 'src/utils/dto/isNotEmptyExceptTags';
+import { isYoutubeLink } from 'src/utils/dto/isYoutubeLink';
 
 export class saveEventDto {
-  @IsNotEmpty({ message: 'タイトルは必須項目です。' })
-  @isNotEmptyExceptTags({ message: '空白のみのタイトルは設定できません。' })
+  @isNotEmptyExceptTags({
+    message: 'タイトルは必須項目です。空白のみは設定できません。',
+  })
   @IsString()
   title: string;
 
@@ -18,7 +26,31 @@ export class saveEventDto {
   description: string;
 
   @ArrayNotEmpty({ message: 'タグは必須項目です。' })
-  @IsArray()
+  @IsArray({ message: '不正なリクエストです。' })
   tags: [];
+
+  @IsNotEmpty({ message: '不正なリクエストです。' })
+  @IsEnum(EventType, { message: '不正なリクエストです。' })
+  type: Date;
+
+  @Type(() => Date)
+  @IsNotEmpty({ message: '不正なリクエストです。' })
+  @IsDate({ message: '不正なリクエストです。' })
+  @MinDate(new Date(), { message: '不正なリクエストです。' })
+  startAt: Date;
+
+  @Type(() => Date)
+  @IsNotEmpty({ message: '不正なリクエストです。' })
+  @IsDate({ message: '不正なリクエストです。' })
+  endAt: Date;
+
+  @IsNotEmpty({ message: '不正なリクエストです。' })
+  @IsBoolean({ message: '不正なリクエストです。' })
+  chatNeeded: boolean;
+
+  @ValidateIf((o, v) => v != null && v.length)
+  @IsArray({ message: '不正なリクエストです。' })
+  @isYoutubeLink({ message: 'Youtubeの動画URLが不正です。ご確認ください。' })
+  videos: [];
 }
 export default saveEventDto;
--- groupware-server/src/utils/dto/isNotEmptyExceptTags.ts
+++ groupware-server/src/utils/dto/isNotEmptyExceptTags.ts
@@ -1,5 +1,6 @@
 import { ValidateBy, ValidationOptions } from 'class-validator';
 
+//中身が空、null、false等、jsのfalse対象、又はスペースのみ、又はリッチテキストエディタ内スペースのみを弾くカスタムバリデーター
 export function isNotEmptyExceptTags(
   validationOptions?: ValidationOptions,
 ): PropertyDecorator {
@@ -8,6 +9,9 @@ export function isNotEmptyExceptTags(
       name: 'isNotEmptyExceptTags',
       validator: {
         validate(value): boolean {
+          if (!value) {
+            return false;
+          }
           const isExist = value
             .replace(/<("[^"]*"|'[^']*'|[^'">])*>|&nbsp;/g, '')
             .trim();
