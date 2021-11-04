6e7e0a13cec3624597f0c1a4ecc817404d3a77af
Kodai Kabasawa <54230093+kbwo@users.noreply.github.com>
2021-11-04 21:51:37 +0900

    Revert "Feature/cu 1q3wu24. CU-1q3wu24[completed]"

--- groupware-frontend/src/pages/wiki/new/index.tsx
+++ groupware-frontend/src/pages/wiki/new/index.tsx
@@ -11,9 +11,6 @@ const CreateQA = () => {
     onSuccess: () => {
       router.push('/wiki/list?page=1&tag=&word=&status=new&type=');
     },
-    onError: () => {
-      alert('Wiki作成が失敗しました。入力内容をご確認ください。');
-    },
   });
 
   return (
