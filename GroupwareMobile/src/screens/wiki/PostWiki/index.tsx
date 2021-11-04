134d58b7b401e72b6595e191de1720b43d6b0f11
Kodai Kabasawa <kabaaa1126@gmail.com>
2021-11-04 14:28:30 +0900

    add validation on post wiki screen CU-1r0edm8[completed] CU-1qfdb9x[completed]

--- GroupwareMobile/src/screens/wiki/PostWiki/index.tsx
+++ GroupwareMobile/src/screens/wiki/PostWiki/index.tsx
@@ -1,15 +1,15 @@
 import {useNavigation, useRoute} from '@react-navigation/native';
-import React, {useRef} from 'react';
+import React, {useEffect, useRef, useState} from 'react';
 import AppHeader from '../../../components/Header';
 import WholeContainer from '../../../components/WholeContainer';
 import {useAPICreateWiki} from '../../../hooks/api/wiki/useAPICreateWiki';
-import {RuleCategory, Wiki, WikiType} from '../../../types';
+import {RuleCategory, TextFormat, Wiki, WikiType} from '../../../types';
 import {
   PostWikiNavigationProps,
   PostWikiRouteProps,
 } from '../../../types/navigator/screenProps/Wiki';
 import {actions, RichEditor, RichToolbar} from 'react-native-pell-rich-editor';
-import {useWindowDimensions} from 'react-native';
+import {Alert, useWindowDimensions} from 'react-native';
 import {
   Input,
   ScrollDiv,
@@ -26,6 +26,12 @@ import {wikiSchema} from '../../../utils/validation/schema';
 import {wikiTypeNameFactory} from '../../../utils/factory/wiki/wikiTypeNameFactory';
 import {DropdownOptionProps} from 'react-native-magnus/lib/typescript/src/ui/dropdown/dropdown.option.type';
 import MarkdownEditorWebView from '../../../components/MarkdownEditorWebView';
+import {useAPIGetTag} from '../../../hooks/api/tag/useAPIGetTag';
+import TagModal from '../../../components/common/TagModal';
+import {useSelectedTags} from '../../../hooks/tag/useSelectedTags';
+import {useTagType} from '../../../hooks/tag/useTagType';
+import {Tag} from 'react-native-magnus';
+import {tagColorFactory} from '../../../utils/factory/tagColorFactory';
 
 const PostWiki: React.FC = () => {
   const navigation = useNavigation<PostWikiNavigationProps>();
@@ -38,11 +44,18 @@ const PostWiki: React.FC = () => {
       navigation.goBack();
     },
   });
+  const {data: tags} = useAPIGetTag();
+  const [visibleTagModal, setVisibleTagModal] = useState(false);
   const {mutate: uploadImage} = useAPIUploadStorage({
     onSuccess: imageUrl => {
       editorRef.current?.insertImage(imageUrl[0]);
     },
   });
+  const {selectedTags, toggleTag, isSelected} = useSelectedTags();
+  const {selectedTagType, selectTagType, filteredTags} = useTagType(
+    'All',
+    tags,
+  );
   const initialValues: Partial<Wiki> = {
     title: '',
     body: '',
@@ -56,6 +69,8 @@ const PostWiki: React.FC = () => {
   const {
     values: newWiki,
     setValues: setNewWiki,
+    errors,
+    touched,
     handleSubmit,
   } = useFormik({
     enableReinitialize: true,
@@ -85,230 +100,290 @@ const PostWiki: React.FC = () => {
   const editorInitializedCallback = () => {
     editorRef.current?.registerToolbar(function () {});
   };
+
+  const handleChangeTextFormat = (format: TextFormat) => {
+    if (newWiki.textFormat === format) {
+      return;
+    }
+    Alert.alert(
+      '入力形式を変更します',
+      '現在の入力状態が失われます\nよろしいですか？',
+      [
+        {
+          text: '変更',
+          onPress: () => {
+            setNewWiki(w => ({
+              ...w,
+              body: '',
+              textFormat: format,
+            }));
+          },
+        },
+        {
+          text: 'キャンセル',
+        },
+      ],
+    );
+  };
+
+  useEffect(() => {
+    setNewWiki(w => ({...w, tags: selectedTags}));
+  }, [selectedTags, setNewWiki]);
+
+  const formatDropdown = (
+    <Dropdown
+      {...defaultDropdownProps}
+      title="入力形式を選択"
+      ref={textFormatDropdownRef}>
+      <Dropdown.Option
+        {...defaultDropdownOptionProps}
+        onPress={() => handleChangeTextFormat('html')}
+        value={'html'}>
+        デフォルト
+      </Dropdown.Option>
+      <Dropdown.Option
+        {...defaultDropdownOptionProps}
+        value={'markdown'}
+        onPress={() => handleChangeTextFormat('markdown')}>
+        マークダウン
+      </Dropdown.Option>
+    </Dropdown>
+  );
+
+  const typeDropdown = (
+    <Dropdown
+      {...defaultDropdownProps}
+      title="タイプを選択"
+      ref={typeDropdownRef}>
+      <Dropdown.Option
+        {...defaultDropdownOptionProps}
+        onPress={() =>
+          setNewWiki(w => ({
+            ...w,
+            type: WikiType.RULES,
+            ruleCategory: RuleCategory.RULES,
+          }))
+        }
+        value={RuleCategory.RULES}>
+        {wikiTypeNameFactory(WikiType.RULES, RuleCategory.RULES)}
+      </Dropdown.Option>
+      <Dropdown.Option
+        {...defaultDropdownOptionProps}
+        onPress={() =>
+          setNewWiki(w => ({
+            ...w,
+            type: WikiType.RULES,
+            ruleCategory: RuleCategory.PHILOSOPHY,
+          }))
+        }
+        value={RuleCategory.PHILOSOPHY}>
+        {wikiTypeNameFactory(WikiType.RULES, RuleCategory.PHILOSOPHY)}
+      </Dropdown.Option>
+      <Dropdown.Option
+        {...defaultDropdownOptionProps}
+        onPress={() =>
+          setNewWiki(w => ({
+            ...w,
+            type: WikiType.RULES,
+            ruleCategory: RuleCategory.ABC,
+          }))
+        }
+        value={RuleCategory.ABC}>
+        {wikiTypeNameFactory(WikiType.RULES, RuleCategory.ABC)}
+      </Dropdown.Option>
+      <Dropdown.Option
+        {...defaultDropdownOptionProps}
+        onPress={() =>
+          setNewWiki(w => ({
+            ...w,
+            type: WikiType.RULES,
+            ruleCategory: RuleCategory.BENEFITS,
+          }))
+        }
+        value={RuleCategory.BENEFITS}>
+        {wikiTypeNameFactory(WikiType.RULES, RuleCategory.BENEFITS)}
+      </Dropdown.Option>
+      <Dropdown.Option
+        {...defaultDropdownOptionProps}
+        onPress={() =>
+          setNewWiki(w => ({
+            ...w,
+            type: WikiType.RULES,
+            ruleCategory: RuleCategory.DOCUMENT,
+          }))
+        }
+        value={RuleCategory.DOCUMENT}>
+        {wikiTypeNameFactory(WikiType.RULES, RuleCategory.DOCUMENT)}
+      </Dropdown.Option>
+      <Dropdown.Option
+        {...defaultDropdownOptionProps}
+        onPress={() =>
+          setNewWiki(w => ({
+            ...w,
+            type: WikiType.QA,
+            ruleCategory: RuleCategory.OTHERS,
+          }))
+        }
+        value={WikiType.QA}>
+        {wikiTypeNameFactory(WikiType.QA)}
+      </Dropdown.Option>
+      <Dropdown.Option
+        {...defaultDropdownOptionProps}
+        onPress={() =>
+          setNewWiki(w => ({
+            ...w,
+            type: WikiType.KNOWLEDGE,
+            ruleCategory: RuleCategory.OTHERS,
+          }))
+        }
+        value={WikiType.KNOWLEDGE}>
+        {wikiTypeNameFactory(WikiType.KNOWLEDGE)}
+      </Dropdown.Option>
+    </Dropdown>
+  );
+
   return (
     <WholeContainer>
       <AppHeader title="Wiki作成" />
+      <TagModal
+        isVisible={visibleTagModal}
+        tags={filteredTags || []}
+        onCloseModal={() => setVisibleTagModal(false)}
+        onPressTag={toggleTag}
+        isSelected={isSelected}
+        selectedTagType={selectedTagType}
+        selectTagType={selectTagType}
+      />
+      {typeDropdown}
+      {formatDropdown}
       <ScrollDiv w={windowWidth * 0.9} alignSelf="center" pt={10}>
+        <Text fontSize={16}>タイトル</Text>
+        {errors.title && touched.title ? (
+          <Text fontSize={16} color="tomato">
+            {errors.title}
+          </Text>
+        ) : null}
         <Input
           placeholder="タイトルを入力してください"
           value={newWiki.title}
           onChangeText={text => setNewWiki(w => ({...w, title: text}))}
           mb="sm"
         />
-        <Div flexDir="row" justifyContent="space-evenly">
-          <Button
-            bg="white"
-            borderWidth={1}
-            borderColor={'#ececec'}
-            p="md"
-            mb="sm"
-            color="black"
-            w={windowWidth * 0.4}
-            onPress={() => typeDropdownRef.current.open()}>
-            {newWiki.type
-              ? wikiTypeNameFactory(newWiki.type, newWiki.ruleCategory)
-              : 'タイプを選択してください'}
-          </Button>
-          <Button
-            bg="white"
-            borderWidth={1}
-            borderColor={'#ececec'}
-            p="md"
-            mb="sm"
-            color="black"
-            w={windowWidth * 0.4}
-            onPress={() => textFormatDropdownRef.current.open()}>
-            {newWiki.textFormat === 'html' ? 'デフォルト' : 'マークダウン'}
-          </Button>
-        </Div>
-        <Div flexDir="row" justifyContent="space-evenly" mb="lg">
-          <Button
-            bg="white"
-            borderWidth={1}
-            borderColor={'#ececec'}
-            color="black"
-            w={windowWidth * 0.4}
-            onPress={() => {}}>
-            タグを選択
-          </Button>
-          <Button
-            bg="pink600"
-            w={windowWidth * 0.4}
-            onPress={() => handleSubmit()}>
-            投稿
-          </Button>
-        </Div>
-        <Dropdown
-          {...defaultDropdownProps}
-          title="タイプを選択"
-          ref={typeDropdownRef}>
-          <Dropdown.Option
-            {...defaultDropdownOptionProps}
-            onPress={() =>
-              setNewWiki(w => ({
-                ...w,
-                type: WikiType.RULES,
-                ruleCategory: RuleCategory.RULES,
-              }))
-            }
-            value={RuleCategory.RULES}>
-            {wikiTypeNameFactory(WikiType.RULES, RuleCategory.RULES)}
-          </Dropdown.Option>
-          <Dropdown.Option
-            {...defaultDropdownOptionProps}
-            onPress={() =>
-              setNewWiki(w => ({
-                ...w,
-                type: WikiType.RULES,
-                ruleCategory: RuleCategory.PHILOSOPHY,
-              }))
-            }
-            value={RuleCategory.PHILOSOPHY}>
-            {wikiTypeNameFactory(WikiType.RULES, RuleCategory.PHILOSOPHY)}
-          </Dropdown.Option>
-          <Dropdown.Option
-            {...defaultDropdownOptionProps}
-            onPress={() =>
-              setNewWiki(w => ({
-                ...w,
-                type: WikiType.RULES,
-                ruleCategory: RuleCategory.ABC,
-              }))
-            }
-            value={RuleCategory.ABC}>
-            {wikiTypeNameFactory(WikiType.RULES, RuleCategory.ABC)}
-          </Dropdown.Option>
-          <Dropdown.Option
-            {...defaultDropdownOptionProps}
-            onPress={() =>
-              setNewWiki(w => ({
-                ...w,
-                type: WikiType.RULES,
-                ruleCategory: RuleCategory.BENEFITS,
-              }))
-            }
-            value={RuleCategory.BENEFITS}>
-            {wikiTypeNameFactory(WikiType.RULES, RuleCategory.BENEFITS)}
-          </Dropdown.Option>
-          <Dropdown.Option
-            {...defaultDropdownOptionProps}
-            onPress={() =>
-              setNewWiki(w => ({
-                ...w,
-                type: WikiType.RULES,
-                ruleCategory: RuleCategory.DOCUMENT,
-              }))
-            }
-            value={RuleCategory.DOCUMENT}>
-            {wikiTypeNameFactory(WikiType.RULES, RuleCategory.DOCUMENT)}
-          </Dropdown.Option>
-          <Dropdown.Option
-            {...defaultDropdownOptionProps}
-            onPress={() =>
-              setNewWiki(w => ({
-                ...w,
-                type: WikiType.QA,
-                ruleCategory: RuleCategory.OTHERS,
-              }))
-            }
-            value={WikiType.QA}>
-            {wikiTypeNameFactory(WikiType.QA)}
-          </Dropdown.Option>
-          <Dropdown.Option
-            {...defaultDropdownOptionProps}
-            onPress={() =>
-              setNewWiki(w => ({
-                ...w,
-                type: WikiType.KNOWLEDGE,
-                ruleCategory: RuleCategory.OTHERS,
-              }))
-            }
-            value={WikiType.KNOWLEDGE}>
-            {wikiTypeNameFactory(WikiType.KNOWLEDGE)}
-          </Dropdown.Option>
-        </Dropdown>
-        <Dropdown
-          {...defaultDropdownProps}
-          title="入力形式を選択"
-          ref={textFormatDropdownRef}>
-          <Dropdown.Option
-            {...defaultDropdownOptionProps}
-            onPress={() =>
-              setNewWiki(w => ({
-                ...w,
-                textFormat: 'html',
-              }))
-            }
-            value={'html'}>
-            デフォルト
-          </Dropdown.Option>
-          <Dropdown.Option
-            {...defaultDropdownOptionProps}
-            value={'markdown'}
-            onPress={() =>
-              setNewWiki(w => ({
-                ...w,
-                textFormat: 'markdown',
-              }))
-            }>
-            マークダウン
-          </Dropdown.Option>
-        </Dropdown>
-        {newWiki.textFormat === 'html' ? (
-          <>
-            <RichToolbar
-              editor={editorRef}
-              selectedIconTint={'#2095F2'}
-              onPressAddImage={async () => {
-                const {formData} = await uploadImageFromGallery();
-                if (formData) {
-                  uploadImage(formData);
-                }
-              }}
-              disabledIconTint={'#bfbfbf'}
-              actions={[
-                actions.heading1,
-                actions.heading2,
-                actions.heading3,
-                actions.heading4,
-                actions.heading5,
-                actions.heading6,
-                'bold',
-                actions.setStrikethrough,
-                actions.insertOrderedList,
-                actions.blockquote,
-                actions.code,
-                actions.insertImage,
-                actions.undo,
-                actions.redo,
-              ]}
-              iconMap={{
-                [actions.heading1]: () => <Text>H1</Text>,
-                [actions.heading2]: () => <Text>H2</Text>,
-                [actions.heading3]: () => <Text>H3</Text>,
-                [actions.heading4]: () => <Text>H4</Text>,
-                [actions.heading5]: () => <Text>H5</Text>,
-                [actions.heading6]: () => <Text>H6</Text>,
-                [actions.bold]: () => <Text fontWeight="bold">B</Text>,
-              }}
-            />
-            <RichEditor
-              placeholder="本文を入力してください"
-              ref={editorRef}
-              style={{height: windowHeight * 0.6}}
-              initialContentHTML={newWiki.body}
-              useContainer={false}
-              editorInitializedCallback={editorInitializedCallback}
-              onChange={text => setNewWiki(w => ({...w, body: text}))}
-            />
-          </>
-        ) : newWiki.textFormat === 'markdown' ? (
-          <Div h={windowHeight * 0.9}>
-            <MarkdownEditorWebView
-              onChange={text => setNewWiki(w => ({...w, body: text}))}
-            />
+        <Div mb={8} flexDir="row" justifyContent="space-evenly">
+          <Div>
+            <Text fontSize={16} mb={4}>
+              タイプを選択
+            </Text>
+            <Button
+              bg="white"
+              borderWidth={1}
+              borderColor={'#ececec'}
+              p="md"
+              color="black"
+              w={windowWidth * 0.4}
+              onPress={() => typeDropdownRef.current.open()}>
+              {newWiki.type
+                ? wikiTypeNameFactory(newWiki.type, newWiki.ruleCategory)
+                : 'タイプを選択してください'}
+            </Button>
           </Div>
+          <Div>
+            <Text fontSize={16} fontWeight="bold" mb={4}>
+              入力形式を選択
+            </Text>
+            <Button
+              bg="white"
+              borderWidth={1}
+              borderColor={'#ececec'}
+              p="md"
+              color="black"
+              w={windowWidth * 0.4}
+              onPress={() => textFormatDropdownRef.current.open()}>
+              {newWiki.textFormat === 'html' ? 'デフォルト' : 'マークダウン'}
+            </Button>
+          </Div>
+        </Div>
+        <Button
+          bg="green600"
+          w={'100%'}
+          mb={8}
+          onPress={() => setVisibleTagModal(true)}>
+          {newWiki.tags?.length
+            ? `${newWiki.tags?.length}個のタグ`
+            : 'タグを選択'}
+        </Button>
+        <Div flexDir="row" flexWrap="wrap" mb={8}>
+          {newWiki.tags?.map(t => (
+            <Tag mr={4} mb={8} color="white" bg={tagColorFactory(t.type)}>
+              {t.name}
+            </Tag>
+          ))}
+        </Div>
+        <Button mb={16} bg="pink600" w={'100%'} onPress={() => handleSubmit()}>
+          投稿
+        </Button>
+        {errors.body && touched.body ? (
+          <Text fontSize={16} color="tomato">
+            {errors.body}
+          </Text>
         ) : null}
+        <Div mb={16}>
+          {newWiki.textFormat === 'html' ? (
+            <>
+              <RichToolbar
+                editor={editorRef}
+                selectedIconTint={'#2095F2'}
+                onPressAddImage={async () => {
+                  const {formData} = await uploadImageFromGallery();
+                  if (formData) {
+                    uploadImage(formData);
+                  }
+                }}
+                disabledIconTint={'#bfbfbf'}
+                actions={[
+                  actions.heading1,
+                  actions.heading2,
+                  actions.heading3,
+                  actions.heading4,
+                  actions.heading5,
+                  actions.heading6,
+                  'bold',
+                  actions.setStrikethrough,
+                  actions.insertOrderedList,
+                  actions.blockquote,
+                  actions.code,
+                  actions.insertImage,
+                  actions.undo,
+                  actions.redo,
+                ]}
+                iconMap={{
+                  [actions.heading1]: () => <Text>H1</Text>,
+                  [actions.heading2]: () => <Text>H2</Text>,
+                  [actions.heading3]: () => <Text>H3</Text>,
+                  [actions.heading4]: () => <Text>H4</Text>,
+                  [actions.heading5]: () => <Text>H5</Text>,
+                  [actions.heading6]: () => <Text>H6</Text>,
+                  [actions.bold]: () => <Text fontWeight="bold">B</Text>,
+                }}
+              />
+              <RichEditor
+                placeholder="本文を入力してください"
+                ref={editorRef}
+                style={{height: windowHeight * 0.6}}
+                initialContentHTML={newWiki.body}
+                useContainer={false}
+                editorInitializedCallback={editorInitializedCallback}
+                onChange={text => setNewWiki(w => ({...w, body: text}))}
+              />
+            </>
+          ) : newWiki.textFormat === 'markdown' ? (
+            <Div h={windowHeight * 0.9}>
+              <MarkdownEditorWebView
+                onChange={text => setNewWiki(w => ({...w, body: text}))}
+              />
+            </Div>
+          ) : null}
+        </Div>
       </ScrollDiv>
     </WholeContainer>
   );
