2956460f42601df2a13876e59c05cfc90112831c
Kodai Kabasawa <54230093+kbwo@users.noreply.github.com>
2021-11-04 21:57:27 +0900

    Revert "Revert "Feature/cu 1q3wu24. CU-1q3wu24[completed]""

---
+++ groupware-server/src/modules/wiki/dto/saveWikiDto.ts
@@ -0,0 +1,13 @@
+import { IsNotEmpty, IsString } from 'class-validator';
+import { isNotEmptyExceptTags } from './custom/isNotEmptyExceptTags';
+
+export class saveWikiDto {
+  @IsNotEmpty()
+  @IsString()
+  title: string;
+
+  @isNotEmptyExceptTags()
+  @IsString()
+  body: string;
+}
+export default saveWikiDto;
