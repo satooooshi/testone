c34af01f6108becc824144feb031dd738f9b6a56
kosei-program-dev <kosei.program.dev@gmail.com>
2021-11-15 18:01:54 +0900

    add entity
    #CU-1r8q1u

---
+++ groupware-server/src/entities/eventIntroduction.entity.ts
@@ -0,0 +1,93 @@
+import {
+  Column,
+  CreateDateColumn,
+  Entity,
+  PrimaryGeneratedColumn,
+  Unique,
+  UpdateDateColumn,
+} from 'typeorm';
+import { EventType } from './event.entity';
+
+@Entity({ name: 'event_introductions' })
+@Unique(['type'])
+export class EventIntroduction {
+  @PrimaryGeneratedColumn()
+  id: number;
+
+  @Column({
+    name: 'type',
+    type: 'enum',
+    enum: EventType,
+    default: EventType.STUDY_MEETING,
+  })
+  type: EventType;
+
+  @Column({
+    type: 'varchar',
+    name: 'title',
+    length: 100,
+    default: '',
+    nullable: false,
+  })
+  title: string;
+
+  @Column({ type: 'longtext', name: 'description' })
+  description: string;
+
+  @Column({
+    type: 'varchar',
+    name: 'image_url',
+    length: 500,
+    nullable: false,
+    default: '',
+  })
+  imageUrl: string;
+
+  @Column({
+    type: 'varchar',
+    name: 'image_url_sub_1',
+    length: 500,
+    nullable: false,
+    default: '',
+  })
+  imageUrlSub1: string;
+
+  @Column({
+    type: 'varchar',
+    name: 'image_url_sub_2',
+    length: 500,
+    nullable: false,
+    default: '',
+  })
+  imageUrlSub2: string;
+
+  @Column({
+    type: 'varchar',
+    name: 'image_url_sub_3',
+    length: 500,
+    nullable: false,
+    default: '',
+  })
+  imageUrlSub3: string;
+
+  @Column({
+    type: 'varchar',
+    name: 'image_url_sub_4',
+    length: 500,
+    nullable: false,
+    default: '',
+  })
+  imageUrlSub4: string;
+
+  @CreateDateColumn({
+    type: 'timestamp',
+    name: 'created_at',
+  })
+  createdAt: Date;
+
+  @UpdateDateColumn({
+    type: 'timestamp',
+    name: 'updated_at',
+  })
+  updatedAt: Date;
+}
