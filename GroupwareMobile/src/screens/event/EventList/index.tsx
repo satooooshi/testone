385cacf068b8c31db1bf7d3135921a3e319a68c1
Kodai Kabasawa <kabaaa1126@gmail.com>
2021-11-17 17:53:58 +0900

    add infinite scroll on event card list CU-1t8jzw7[completed]

--- GroupwareMobile/src/screens/event/EventList/index.tsx
+++ GroupwareMobile/src/screens/event/EventList/index.tsx
@@ -1,4 +1,4 @@
-import React, {useState} from 'react';
+import React, {useEffect, useState} from 'react';
 import WholeContainer from '../../../components/WholeContainer';
 import AppHeader, {Tab} from '../../../components/Header';
 import {EventListProps} from '../../../types/navigator/screenProps/Event';
@@ -18,20 +18,29 @@ import {defaultWeekQuery} from '../../../utils/eventQueryRefresh';
 import EventFormModal from '../../../components/events/EventFormModal';
 import {useAPIGetUsers} from '../../../hooks/api/user/useAPIGetUsers';
 import {useAPICreateEvent} from '../../../hooks/api/event/useAPICreateEvent';
+import {ActivityIndicator} from 'react-native';
+import {Overlay} from 'react-native-magnus';
 
 const TopTab = createMaterialTopTabNavigator();
 
 const EventList: React.FC<EventListProps> = ({navigation}) => {
   const [visibleSearchFormModal, setVisibleSearchFormModal] = useState(false);
-  const {data: tags} = useAPIGetTag();
-  const {data: users} = useAPIGetUsers();
+  const {data: tags, isLoading: isLoadingGetTags} = useAPIGetTag();
+  const {data: users, isLoading: isLoadingGetUsers} = useAPIGetUsers();
   const [searchQuery, setSearchQuery] = useState<SearchQueryToGetEvents>(
     defaultWeekQuery(),
   );
-  const {data: events, refetch: refetchEvents} =
-    useAPIGetEventList(searchQuery);
+  const {
+    data: events,
+    refetch: refetchEvents,
+    isLoading: isLoadingGetEventList,
+  } = useAPIGetEventList(searchQuery);
+  const [eventsForInfinitScroll, setEventsForInfinitSroll] = useState(
+    events?.events || [],
+  );
   const [visibleEventFormModal, setEventFormModal] = useState(false);
-  const {mutate: saveEvent} = useAPICreateEvent({
+  const [screenLoading, setScreenLoading] = useState(false);
+  const {mutate: saveEvent, isLoading: isLoadingSaveEvent} = useAPICreateEvent({
     onSuccess: () => {
       setEventFormModal(false);
       refetchEvents();
@@ -40,31 +49,32 @@ const EventList: React.FC<EventListProps> = ({navigation}) => {
   const tabs: Tab[] = [
     {
       name: 'All',
-      onPress: () => queryRefresh({type: undefined}),
+      onPress: () => queryRefresh({page: '1', type: undefined}),
     },
     {
       name: eventTypeNameFactory(EventType.IMPRESSIVE_UNIVERSITY),
-      onPress: () => queryRefresh({type: EventType.IMPRESSIVE_UNIVERSITY}),
+      onPress: () =>
+        queryRefresh({page: '1', type: EventType.IMPRESSIVE_UNIVERSITY}),
     },
     {
       name: eventTypeNameFactory(EventType.STUDY_MEETING),
-      onPress: () => queryRefresh({type: EventType.STUDY_MEETING}),
+      onPress: () => queryRefresh({page: '1', type: EventType.STUDY_MEETING}),
     },
     {
       name: eventTypeNameFactory(EventType.BOLDAY),
-      onPress: () => queryRefresh({type: EventType.BOLDAY}),
+      onPress: () => queryRefresh({page: '1', type: EventType.BOLDAY}),
     },
     {
       name: eventTypeNameFactory(EventType.COACH),
-      onPress: () => queryRefresh({type: EventType.COACH}),
+      onPress: () => queryRefresh({page: '1', type: EventType.COACH}),
     },
     {
       name: eventTypeNameFactory(EventType.CLUB),
-      onPress: () => queryRefresh({type: EventType.CLUB}),
+      onPress: () => queryRefresh({page: '1', type: EventType.CLUB}),
     },
     {
       name: eventTypeNameFactory(EventType.SUBMISSION_ETC),
-      onPress: () => queryRefresh({type: EventType.SUBMISSION_ETC}),
+      onPress: () => queryRefresh({page: '1', type: EventType.SUBMISSION_ETC}),
     },
   ];
 
@@ -82,6 +92,47 @@ const EventList: React.FC<EventListProps> = ({navigation}) => {
     typeof searchQuery.from !== undefined &&
     typeof searchQuery.to !== undefined;
 
+  useEffect(() => {
+    if (events?.events && events?.events.length) {
+      setEventsForInfinitSroll(e => {
+        if (e.length) {
+          return [...e, ...events.events];
+        }
+        return events.events;
+      });
+    }
+  }, [events?.events]);
+
+  useEffect(() => {
+    if (
+      isLoadingGetEventList ||
+      isLoadingGetUsers ||
+      isLoadingGetTags ||
+      isLoadingSaveEvent
+    ) {
+      setScreenLoading(true);
+      return;
+    }
+    setScreenLoading(false);
+  }, [
+    isLoadingGetEventList,
+    isLoadingGetTags,
+    isLoadingGetUsers,
+    isLoadingSaveEvent,
+  ]);
+
+  useEffect(() => {
+    if (!isCalendar) {
+      setEventsForInfinitSroll([]);
+    }
+  }, [
+    searchQuery.word,
+    searchQuery.status,
+    searchQuery.type,
+    searchQuery.tag,
+    isCalendar,
+  ]);
+
   return (
     <WholeContainer>
       <AppHeader
@@ -93,6 +144,9 @@ const EventList: React.FC<EventListProps> = ({navigation}) => {
         rightButtonName="新規イベント"
         onPressRightButton={() => setEventFormModal(true)}
       />
+      <Overlay visible={screenLoading} p="xl">
+        <ActivityIndicator />
+      </Overlay>
       <EventFormModal
         isVisible={visibleEventFormModal}
         onCloseModal={() => setEventFormModal(false)}
@@ -150,7 +204,7 @@ const EventList: React.FC<EventListProps> = ({navigation}) => {
           name="FutureEvents"
           children={() => (
             <EventCardList
-              searchResult={events}
+              searchResult={eventsForInfinitScroll}
               status="future"
               navigation={navigation}
               searchQuery={searchQuery}
@@ -163,7 +217,7 @@ const EventList: React.FC<EventListProps> = ({navigation}) => {
           name="PastEvents"
           children={() => (
             <EventCardList
-              searchResult={events}
+              searchResult={eventsForInfinitScroll}
               status="past"
               navigation={navigation}
               searchQuery={searchQuery}
@@ -176,7 +230,7 @@ const EventList: React.FC<EventListProps> = ({navigation}) => {
           name="CurrentEvents"
           children={() => (
             <EventCardList
-              searchResult={events}
+              searchResult={eventsForInfinitScroll}
               status="current"
               navigation={navigation}
               searchQuery={searchQuery}
