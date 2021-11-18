f950f14650bc3bdca4242c8ed5139c4f7c52df84
Kodai Kabasawa <kabaaa1126@gmail.com>
2021-11-18 20:35:39 +0900

    fix error in markdown editor CU-1te7rhv[completed]

--- GroupwareMobile/src/components/MarkdownEditorWebView/index.tsx
+++ GroupwareMobile/src/components/MarkdownEditorWebView/index.tsx
@@ -1,4 +1,5 @@
-import React, {useRef} from 'react';
+import {useIsFocused} from '@react-navigation/native';
+import React, {useEffect, useRef} from 'react';
 import WebView from 'react-native-webview';
 import {baseURL, jwtFormDataHeader, markdownEditorURL} from '../../utils/url';
 import {uploadStorageURL} from '../../utils/url/storage.url';
@@ -12,20 +13,29 @@ const MarkdownEditorWebView: React.FC<MarkdownEditorProps> = ({
   onChange,
   value,
 }) => {
-  const injectedJavaScript: string = `
-  window.UPLOAD_STORAGE_URL = '${baseURL}${uploadStorageURL}';
-  window.JWT_HEADER = ${JSON.stringify(jwtFormDataHeader)};
-  window.INIT_TEXT = '${value || ''}';
-  `;
+  const isFocused = useIsFocused();
   const webViewRef = useRef<WebView | null>(null);
+
+  useEffect(() => {
+    setTimeout(() => {
+      webViewRef.current?.postMessage(
+        JSON.stringify({
+          url: `${baseURL}${uploadStorageURL}`,
+          header: jwtFormDataHeader,
+          initText: `${value}`,
+        }),
+      );
+    }, 500);
+  }, [isFocused, value]);
+
   return (
     <WebView
+      cacheEnabled={false}
       scrollEnabled={false}
       ref={webViewRef}
       source={{
         uri: markdownEditorURL,
       }}
-      injectedJavaScriptBeforeContentLoaded={injectedJavaScript}
       onMessage={event => {
         const markdownText = event.nativeEvent.data as string;
         onChange(markdownText);
