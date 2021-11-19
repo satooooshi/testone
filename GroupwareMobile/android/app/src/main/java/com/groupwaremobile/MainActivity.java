2b572130571ecbb863942ddfcaa5516ad3616c07
Kodai Kabasawa <kabaaa1126@gmail.com>
2021-11-19 15:30:26 +0900

    setup for android. enable to build CU-1tec4um[completed]

--- GroupwareMobile/android/app/src/main/java/com/groupwaremobile/MainActivity.java
+++ GroupwareMobile/android/app/src/main/java/com/groupwaremobile/MainActivity.java
@@ -1,8 +1,17 @@
 package com.groupwaremobile;
 
+//react-navigation
+import android.os.Bundle;
+
 import com.facebook.react.ReactActivity;
 
 public class MainActivity extends ReactActivity {
+
+  //react-navigation
+  @Override
+  protected void onCreate(Bundle savedInstanceState) {
+    super.onCreate(null);
+  }
 
   /**
    * Returns the name of the main component registered from JavaScript. This is used to schedule
