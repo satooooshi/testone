cd8d5eb37886b90482040bda461508679c79a9bb
Kodai Kabasawa <kabaaa1126@gmail.com>
2021-11-19 12:13:49 +0900

    hotfix to edit markdown wiki correctly

--- groupware-frontend/src/templates/WikiForm/index.tsx
+++ groupware-frontend/src/templates/WikiForm/index.tsx
@@ -198,14 +198,18 @@ const WikiForm: React.FC<WikiFormProps> = ({
         setEditorState(EditorState.createWithContent(stateFromHTML(wiki.body)));
       }
     }
-  }, [wiki]);
+  }, [setNewQuestion, wiki]);
 
   useEffect(() => {
     if (editorState) {
-      setNewQuestion((q) => ({
-        ...q,
-        body: stateToHTML(editorState.getCurrentContent()),
-      }));
+      setNewQuestion((q) =>
+        q.textFormat === 'html'
+          ? {
+              ...q,
+              body: stateToHTML(editorState.getCurrentContent()),
+            }
+          : q,
+      );
     }
   }, [editorState, setNewQuestion, wiki]);
 
