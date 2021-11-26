53fcfe131b6484c26d5a358d2cf11738af7de112
Kodai Kabasawa <kabaaa1126@gmail.com>
2021-11-26 19:01:39 +0900

    add custom hook for listing album images CU-1vrn5tg[completed]

--- GroupwareMobile/src/utils/url/chat.url.ts
+++ GroupwareMobile/src/utils/url/chat.url.ts
@@ -10,6 +10,8 @@ export const saveChatGroupURL = `/${chatURL}/save-chat-group`;
 export const saveLastReadChatTimeURL = `/${chatURL}/save-last-read-chat-time`;
 export const leaveChatRoomURL = `/${chatURL}/leave-room`;
 export const noteURL = (roomId: string) => `${chatURL}/v2/room/${roomId}/note`;
+export const albumURL = (roomId: string) =>
+  `${chatURL}/v2/room/${roomId}/album`;
 
 //GET
 export const getChatNotesURL = (roomId: string, page: string) =>
@@ -17,3 +19,12 @@ export const getChatNotesURL = (roomId: string, page: string) =>
 //DELETE
 export const chatNoteDetailURL = (roomId: string, noteId: string) =>
   `${noteURL(roomId)}/${noteId}`;
+//GET
+export const getChatAlbumsURL = (roomId: string, page: string) =>
+  `${noteURL(roomId)}?page=${page}`;
+//DELETE
+export const chatAlbumImageURL = (
+  roomId: string,
+  albumId: string,
+  page: string,
+) => `${albumURL(roomId)}/${albumId}/page=${page}`;
