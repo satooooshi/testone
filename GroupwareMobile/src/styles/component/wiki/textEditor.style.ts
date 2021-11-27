37d8f7d696fdd22c2d3ae1b93d6e4fa79cbc0fb4
Shinnosuke-Takagi <s.takagi5664@gmail.com>
2021-11-28 06:06:10 +0900

    Implement to be able to display answers list at WikiDetail page
    feature/CU-1vrmeff[completed]

---
+++ GroupwareMobile/src/styles/component/wiki/textEditor.style.ts
@@ -0,0 +1,10 @@
+import {StyleSheet} from 'react-native';
+
+export const textEditorStyles = StyleSheet.create({
+  richEditor: {
+    minHeight: 300,
+    flex: 1,
+    borderBottomWidth: StyleSheet.hairlineWidth,
+    borderColor: '#e3e3e3',
+  },
+});
