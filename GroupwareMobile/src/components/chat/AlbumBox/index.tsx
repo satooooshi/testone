ac5a0c7e40b3860feb325368fa597671fbedc80c
Kodai Kabasawa <kabaaa1126@gmail.com>
2021-11-29 15:35:07 +0900

    add component for listing album CU-1vybxhe[completed]

---
+++ GroupwareMobile/src/components/chat/AlbumBox/index.tsx
@@ -0,0 +1,49 @@
+import React from 'react';
+import {TouchableHighlight, useWindowDimensions} from 'react-native';
+import {Div, Image, Text} from 'react-native-magnus';
+import {ChatAlbum} from '../../../types';
+import {darkFontColor} from '../../../utils/colors';
+
+type AlbumBoxProps = {
+  onPress: () => void;
+  album: ChatAlbum;
+};
+
+const AlbumBox: React.FC<AlbumBoxProps> = ({onPress, album}) => {
+  const {width: windowWidth} = useWindowDimensions();
+
+  return (
+    <TouchableHighlight underlayColor="none" onPress={onPress}>
+      <Div bg="white" alignItems="center" py="lg">
+        <Div w={windowWidth * 0.9}>
+          <Div mb={'sm'}>
+            {album?.images?.length ? (
+              <Image
+                alignSelf="center"
+                rounded="md"
+                source={{uri: album.images[0].imageURL}}
+                w={'100%'}
+                h={windowWidth * 0.9}
+              />
+            ) : (
+              <Image
+                rounded="md"
+                source={require('../../../../assets/no-image.jpg')}
+                w={'100%'}
+                h={windowWidth * 0.9}
+              />
+            )}
+          </Div>
+          <Text fontWeight={'bold'} fontSize={18} mb="sm">
+            {album.title}
+          </Text>
+          <Text fontSize={14} color={darkFontColor}>
+            {album.images?.length || '0'}
+          </Text>
+        </Div>
+      </Div>
+    </TouchableHighlight>
+  );
+};
+
+export default AlbumBox;
