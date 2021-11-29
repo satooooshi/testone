58f076af6a136d4cbe5f140f440c9dc0de97b790
Shinnosuke-Takagi <s.takagi5664@gmail.com>
2021-11-29 19:49:30 +0900

    Improve PostAnswer component to be able to display wiki detail
    feature/CU-1vrmeff

--- GroupwareMobile/src/screens/wiki/PostAnswer/index.tsx
+++ GroupwareMobile/src/screens/wiki/PostAnswer/index.tsx
@@ -6,7 +6,7 @@ import {answerSchema} from '../../../utils/validation/schema';
 import {useAPIUploadStorage} from '../../../hooks/api/storage/useAPIUploadStorage';
 import {uploadImageFromGallery} from '../../../utils/cropImage/uploadImageFromGallery';
 import {ActivityIndicator, useWindowDimensions} from 'react-native';
-import {Button, Overlay, ScrollDiv, Text} from 'react-native-magnus';
+import {Button, Div, Overlay, ScrollDiv, Text} from 'react-native-magnus';
 import {
   PostWikiNavigationProps,
   PostAnswerRouteProps,
@@ -16,12 +16,15 @@ import {useAPIGetWikiDetail} from '../../../hooks/api/wiki/useAPIGetWikiDetail';
 import TextEditor from '../../../components/wiki/TextEditor';
 import WholeContainer from '../../../components/WholeContainer';
 import HeaderWithTextButton from '../../../components/Header';
+import RenderHtml from 'react-native-render-html';
+import MarkdownIt from 'markdown-it';
 
 const PostAnswer: React.FC = () => {
   const navigation = useNavigation<PostWikiNavigationProps>();
   const route = useRoute<PostAnswerRouteProps>();
   const wikiId = route.params.id;
   const {data: wikiInfo} = useAPIGetWikiDetail(wikiId);
+  const mdParser = new MarkdownIt({breaks: true});
   const {mutate: saveAnswer, isLoading: loadingSaveAnswer} = useAPICreateAnswer(
     {
       onSuccess: () => {
@@ -31,7 +34,7 @@ const PostAnswer: React.FC = () => {
   );
   const {mutate: uploadImage, isLoading: loadingUploadImage} =
     useAPIUploadStorage();
-  const {width: windowWidth} = useWindowDimensions();
+  const {width: windowWidth, height: windowHeight} = useWindowDimensions();
   const isLoading = loadingSaveAnswer || loadingUploadImage;
   const initialValues: Partial<QAAnswer> = {
     body: '',
@@ -61,33 +64,63 @@ const PostAnswer: React.FC = () => {
 
   return (
     <WholeContainer>
-      <HeaderWithTextButton
-        title={wikiInfo?.answers?.length ? '回答を追加する' : '回答を投稿する'}
-      />
-      <Overlay visible={isLoading} p="xl">
-        <ActivityIndicator />
-      </Overlay>
-      <ScrollDiv
-        nestedScrollEnabled={true}
-        scrollEventThrottle={20}
-        keyboardDismissMode={'none'}
-        w={windowWidth * 0.9}
-        alignSelf="center"
-        pt={10}>
-        <Button mb={16} bg="pink600" w={'100%'} onPress={() => handleSubmit()}>
-          投稿
-        </Button>
-        {errors.body && touched.body ? (
-          <Text fontSize={16} color="tomato">
-            {errors.body}
-          </Text>
-        ) : null}
-        <TextEditor
-          textFormat={wikiInfo?.textFormat}
-          onUploadImage={handleUploadImage}
-          onChange={text => setNewAnswer(a => ({...a, body: text}))}
-        />
-      </ScrollDiv>
+      {wikiInfo ? (
+        <>
+          <HeaderWithTextButton
+            title={
+              wikiInfo.answers?.length ? '回答を追加する' : '回答を投稿する'
+            }
+          />
+          <Overlay visible={isLoading} p="xl">
+            <ActivityIndicator />
+          </Overlay>
+          <ScrollDiv
+            nestedScrollEnabled={true}
+            scrollEventThrottle={20}
+            keyboardDismissMode={'none'}
+            w={windowWidth * 0.9}
+            h={windowHeight * 0.3}
+            alignSelf="center"
+            pt={10}
+            bg="white"
+            rounded="md"
+            mb={16}>
+            <RenderHtml
+              source={{
+                html:
+                  wikiInfo.textFormat === 'html'
+                    ? wikiInfo.body
+                    : mdParser.render(wikiInfo.body),
+              }}
+            />
+          </ScrollDiv>
+          <ScrollDiv
+            nestedScrollEnabled={true}
+            scrollEventThrottle={20}
+            keyboardDismissMode={'none'}
+            w={windowWidth * 0.9}
+            alignSelf="center"
+            pt={10}>
+            <Button
+              mb={16}
+              bg="pink600"
+              w={'100%'}
+              onPress={() => handleSubmit()}>
+              投稿
+            </Button>
+            {errors.body && touched.body ? (
+              <Text fontSize={16} color="tomato">
+                {errors.body}
+              </Text>
+            ) : null}
+            <TextEditor
+              textFormat={wikiInfo.textFormat}
+              onUploadImage={handleUploadImage}
+              onChange={text => setNewAnswer(a => ({...a, body: text}))}
+            />
+          </ScrollDiv>
+        </>
+      ) : null}
     </WholeContainer>
   );
 };
