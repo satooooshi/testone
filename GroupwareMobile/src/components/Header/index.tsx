f9a906259e199a3c25a3ea024b47c3a3f53dc936
Kodai Kabasawa <kabaaa1126@gmail.com>
2021-11-30 11:47:56 +0900

    organize imports

--- GroupwareMobile/src/components/Header/index.tsx
+++ GroupwareMobile/src/components/Header/index.tsx
@@ -1,11 +1,6 @@
 import React, {useMemo} from 'react';
-import {Text, Div, Button, Icon} from 'react-native-magnus';
-import {darkFontColor, blueColor} from '../../utils/colors';
-import FastImage from 'react-native-fast-image';
-import {FlatList, TouchableOpacity} from 'react-native';
-import {headerStyles} from '../../styles/component/header.style';
-import {DrawerActions, useNavigation} from '@react-navigation/native';
-import Ionicons from 'react-native-vector-icons/Ionicons';
+import {Button} from 'react-native-magnus';
+import {blueColor} from '../../utils/colors';
 import HeaderTemplate, {HeaderTemplateProps} from './HeaderTemplate';
 
 export type Tab = {
@@ -20,16 +15,7 @@ export type HeaderWithTextButtonProps = HeaderTemplateProps & {
 };
 
 const HeaderWithTextButton: React.FC<HeaderWithTextButtonProps> = props => {
-  const {
-    title,
-    activeTabName,
-    tabs,
-    rightButtonName,
-    onPressRightButton,
-    enableBackButton = false,
-    screenForBack,
-  } = props;
-  const navigation = useNavigation();
+  const {rightButtonName, onPressRightButton} = props;
   const boolToDisplayRightButton = useMemo(() => {
     return rightButtonName && onPressRightButton;
   }, [onPressRightButton, rightButtonName]);
