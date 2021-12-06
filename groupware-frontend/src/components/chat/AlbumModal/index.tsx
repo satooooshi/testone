e6faa1720bb33f57adaf3eaa1cced3e5faa7741a
Kodai Kabasawa <kabaaa1126@gmail.com>
2021-12-06 17:15:14 +0900

    add feature for image viewer

--- groupware-frontend/src/components/chat/AlbumModal/index.tsx
+++ groupware-frontend/src/components/chat/AlbumModal/index.tsx
@@ -12,11 +12,15 @@ import {
   SimpleGrid,
   Text,
 } from '@chakra-ui/react';
-import React, { useRef } from 'react';
+import React, { useMemo, useRef, useState } from 'react';
 import { ChatAlbum, ChatAlbumImage } from 'src/types';
 import noImage from '@/public/no-image.jpg';
 import NextImage from 'next/image';
 import { AiOutlineLeft } from 'react-icons/ai';
+import dynamic from 'next/dynamic';
+
+const Viewer = dynamic(() => import('react-viewer'), { ssr: false });
+import { ImageDecorator } from 'react-viewer/lib/ViewerProps';
 
 type AlbumModalProps = {
   isOpen: boolean;
@@ -42,110 +46,156 @@ const AlbumModal: React.FC<AlbumModalProps> = ({
   onUploadImage,
 }) => {
   const imageUploaderRef = useRef<HTMLInputElement | null>(null);
+  const [selectedImage, setSelectedImage] = useState<ChatAlbumImage>();
+  const imagesInViewer = useMemo((): ImageDecorator[] => {
+    if (selectedAlbum) {
+      return (
+        selectedAlbum.images?.map((i) => ({
+          src: i.imageURL || '',
+          alt: 'アルバム画像',
+          downloadUrl: i.imageURL || '',
+        })) || []
+      );
+    } else if (selectedImage) {
+      return [
+        {
+          src: selectedImage.imageURL || '',
+          alt: 'アルバム画像',
+          downloadUrl: selectedImage.imageURL || '',
+        },
+      ];
+    } else {
+      return [];
+    }
+  }, [selectedAlbum, selectedImage]);
+  const activeIndex = useMemo(() => {
+    if (selectedImage) {
+      const isNowUri = (element: ImageDecorator) =>
+        element.src === selectedImage?.imageURL;
+      return imagesInViewer.findIndex(isNowUri) + 1;
+    }
+  }, [imagesInViewer, selectedImage]);
 
   return (
-    <Modal onClose={onClose} isOpen={isOpen} scrollBehavior="inside">
-      <ModalOverlay />
-      <ModalContent h="80vh" bg={'#f9fafb'}>
-        <ModalHeader>{headerName}</ModalHeader>
-        <ModalCloseButton />
-        <ModalBody>
-          {!selectedAlbum ? (
-            albums.map((a) => (
-              <Box mb="sm" key={a.id}>
-                <Link
-                  bg="white"
-                  borderColor="gray.300"
-                  borderWidth={1}
-                  borderRadius="md"
-                  w={'100%'}
-                  maxWidth={'400px'}
-                  display="flex"
-                  flexDir="column"
-                  onClick={() => onClickAlbum(a)}
-                  alignItems="center">
-                  <Box
-                    p="8px"
-                    flexDir="row"
+    <>
+      <Viewer
+        images={imagesInViewer}
+        visible={!!selectedImage}
+        onClose={() => setSelectedImage(undefined)}
+        downloadable={true}
+        downloadInNewWindow={true}
+        activeIndex={activeIndex}
+      />
+      <Modal
+        onClose={onClose}
+        isOpen={isOpen && !selectedImage}
+        scrollBehavior="inside">
+        <ModalOverlay />
+        <ModalContent h="80vh" bg={'#f9fafb'}>
+          <ModalHeader>{headerName}</ModalHeader>
+          <ModalCloseButton />
+          <ModalBody>
+            {!selectedAlbum ? (
+              albums.map((a) => (
+                <Box mb="sm" key={a.id}>
+                  <Link
+                    bg="white"
+                    borderColor="gray.300"
+                    borderWidth={1}
+                    borderRadius="md"
                     w={'100%'}
-                    justifyContent="flex-start">
-                    <Text noOfLines={1} mr="4px" display="inline">
-                      {a.title}
-                    </Text>
-                  </Box>
-                  {a.images?.length ? (
-                    <Image
-                      src={a.images[0].imageURL}
-                      alt="アルバム画像"
+                    maxWidth={'400px'}
+                    display="flex"
+                    flexDir="column"
+                    onClick={() => onClickAlbum(a)}
+                    alignItems="center">
+                    <Box
+                      p="8px"
+                      flexDir="row"
                       w={'100%'}
-                      h={'200px'}
-                    />
-                  ) : (
-                    <NextImage src={noImage} alt="アルバム画像" />
-                  )}
-                </Link>
-              </Box>
-            ))
-          ) : (
-            <Box>
-              <Box flexDir="row" justifyContent="space-between" display="flex">
-                <Button
-                  size="sm"
+                      justifyContent="flex-start">
+                      <Text noOfLines={1} mr="4px" display="inline">
+                        {a.title}
+                      </Text>
+                    </Box>
+                    {a.images?.length ? (
+                      <Image
+                        src={a.images[0].imageURL}
+                        alt="アルバム画像"
+                        w={'100%'}
+                        h={'200px'}
+                      />
+                    ) : (
+                      <NextImage src={noImage} alt="アルバム画像" />
+                    )}
+                  </Link>
+                </Box>
+              ))
+            ) : (
+              <Box>
+                <Box
                   flexDir="row"
-                  onClick={onClickBackButton}
-                  mb="8px"
-                  alignItems="center">
-                  <AiOutlineLeft size={24} style={{ display: 'inline' }} />
-                  <Text display="inline">一覧へ戻る</Text>
-                </Button>
-                <Button
-                  size="sm"
-                  flexDir="row"
-                  onClick={() => imageUploaderRef.current?.click()}
-                  mb="8px"
-                  colorScheme="green"
-                  alignItems="center">
-                  <Text display="inline">写真を追加</Text>
-                </Button>
-                <input
-                  multiple
-                  ref={imageUploaderRef}
-                  accept="image/*"
-                  type="file"
-                  hidden
-                  name="imageUploadToAlbum"
-                  onChange={() => {
-                    const files = imageUploaderRef.current?.files;
-                    const fileArr: File[] = [];
-                    if (!files) {
-                      return;
-                    }
-                    for (let i = 0; i < files.length; i++) {
-                      const renamedFile = new File([files[i]], files[i].name, {
-                        type: files[i].type,
-                        lastModified: files[i].lastModified,
-                      });
-                      fileArr.push(renamedFile);
-                    }
-                    onUploadImage(fileArr);
-                  }}
-                />
-              </Box>
-              <SimpleGrid spacing="8px" columns={2}>
-                {images?.map((i) => (
-                  <Image
-                    key={i.id}
-                    src={i.imageURL}
-                    alt="アルバム画像"
-                    w="100%"
+                  justifyContent="space-between"
+                  display="flex">
+                  <Button
+                    size="sm"
+                    flexDir="row"
+                    onClick={onClickBackButton}
+                    mb="8px"
+                    alignItems="center">
+                    <AiOutlineLeft size={24} style={{ display: 'inline' }} />
+                    <Text display="inline">一覧へ戻る</Text>
+                  </Button>
+                  <Button
+                    size="sm"
+                    flexDir="row"
+                    onClick={() => imageUploaderRef.current?.click()}
+                    mb="8px"
+                    colorScheme="green"
+                    alignItems="center">
+                    <Text display="inline">写真を追加</Text>
+                  </Button>
+                  <input
+                    multiple
+                    ref={imageUploaderRef}
+                    accept="image/*"
+                    type="file"
+                    hidden
+                    name="imageUploadToAlbum"
+                    onChange={() => {
+                      const files = imageUploaderRef.current?.files;
+                      const fileArr: File[] = [];
+                      if (!files) {
+                        return;
+                      }
+                      for (let i = 0; i < files.length; i++) {
+                        const renamedFile = new File(
+                          [files[i]],
+                          files[i].name,
+                          {
+                            type: files[i].type,
+                            lastModified: files[i].lastModified,
+                          },
+                        );
+                        fileArr.push(renamedFile);
+                      }
+                      onUploadImage(fileArr);
+                    }}
                   />
-                ))}
-              </SimpleGrid>
-            </Box>
-          )}
-        </ModalBody>
-      </ModalContent>
-    </Modal>
+                </Box>
+                <SimpleGrid spacing="8px" columns={2}>
+                  {images?.map((i) => (
+                    <Link key={i.id} onClick={() => setSelectedImage(i)}>
+                      <Image src={i.imageURL} alt="アルバム画像" w="100%" />
+                    </Link>
+                  ))}
+                </SimpleGrid>
+              </Box>
+            )}
+          </ModalBody>
+        </ModalContent>
+      </Modal>
+    </>
   );
 };
 
--- groupware-frontend/src/components/chat/NoteModal/index.tsx
+++ groupware-frontend/src/components/chat/NoteModal/index.tsx
@@ -4,6 +4,7 @@ import {
   Button,
   IconButton,
   Image,
+  Link,
   Menu,
   MenuButton,
   MenuItem,
@@ -18,13 +19,17 @@ import {
   Text,
   Textarea,
 } from '@chakra-ui/react';
-import React, { useEffect, useRef } from 'react';
-import { ChatNote } from 'src/types';
+import React, { useEffect, useMemo, useRef, useState } from 'react';
+import { ChatNote, ChatNoteImage } from 'src/types';
 import { AiOutlineLeft } from 'react-icons/ai';
 import { userNameFactory } from 'src/utils/factory/userNameFactory';
 import { dateTimeFormatterFromJSDDate } from 'src/utils/dateTimeFormatter';
 import { FiMenu } from 'react-icons/fi';
 import { useFormik } from 'formik';
+import { ImageDecorator } from 'react-viewer/lib/ViewerProps';
+import dynamic from 'next/dynamic';
+
+const Viewer = dynamic(() => import('react-viewer'), { ssr: false });
 
 type NoteModalProps = {
   isOpen: boolean;
@@ -52,6 +57,32 @@ const NoteModal: React.FC<NoteModalProps> = ({
   onSubmitEdittedNote,
 }) => {
   const imageUploaderRef = useRef<HTMLInputElement | null>(null);
+  const [selectedImage, setSelectedImage] = useState<Partial<ChatNoteImage>>();
+  const imagesInViewer = useMemo((): ImageDecorator[] => {
+    if (edittedNote) {
+      return (
+        edittedNote.images?.map((i) => ({
+          src: i.imageURL || '',
+          alt: 'アルバム画像',
+          downloadUrl: i.imageURL || '',
+        })) || []
+      );
+    } else if (selectedImage) {
+      return [
+        {
+          src: selectedImage.imageURL || '',
+          alt: 'アルバム画像',
+          downloadUrl: selectedImage.imageURL || '',
+        },
+      ];
+    } else {
+      return [];
+    }
+  }, [edittedNote, selectedImage]);
+
+  const isNowUri = (element: ImageDecorator) =>
+    element.src === selectedImage?.imageURL;
+  const activeIndex = imagesInViewer.findIndex(isNowUri);
 
   const initialValues: Partial<ChatNote> = {
     content: '',
@@ -75,156 +106,175 @@ const NoteModal: React.FC<NoteModalProps> = ({
   }, [edittedNote, setValues]);
 
   return (
-    <Modal onClose={onClose} isOpen={isOpen} scrollBehavior="inside">
-      <ModalOverlay />
-      <ModalContent h="90vh" bg={'#f9fafb'}>
-        <ModalHeader>{headerName}</ModalHeader>
-        <ModalCloseButton />
-        <ModalBody>
-          {!edittedNote ? (
-            notes.map((n) => (
-              <Box
-                mb="16px"
-                p={'4px'}
-                key={n.id}
-                bg="white"
-                borderColor="gray.300"
-                borderWidth={1}
-                borderRadius="md"
-                w={'100%'}
-                maxWidth={'400px'}
-                display="flex"
-                flexDir="column"
-                alignItems="center">
+    <>
+      <Viewer
+        images={imagesInViewer}
+        visible={!!selectedImage}
+        onClose={() => setSelectedImage(undefined)}
+        downloadable={true}
+        downloadInNewWindow={true}
+        activeIndex={activeIndex}
+      />
+      <Modal
+        onClose={onClose}
+        isOpen={isOpen && !selectedImage}
+        scrollBehavior="inside">
+        <ModalOverlay />
+        <ModalContent h="90vh" bg={'#f9fafb'}>
+          <ModalHeader>{headerName}</ModalHeader>
+          <ModalCloseButton />
+          <ModalBody>
+            {!edittedNote ? (
+              notes.map((n) => (
                 <Box
-                  mb="8px"
-                  flexDir="row"
+                  mb="16px"
+                  p={'4px'}
+                  key={n.id}
+                  bg="white"
+                  borderColor="gray.300"
+                  borderWidth={1}
+                  borderRadius="md"
+                  w={'100%'}
+                  maxWidth={'400px'}
                   display="flex"
-                  alignItems="center"
-                  w="100%"
-                  justifyContent="space-between">
-                  <Box flexDir="row" display="flex">
-                    <Avatar
-                      src={
-                        n.editors?.length ? n.editors[0].avatarUrl : undefined
-                      }
-                      size="md"
-                      mr="4px"
-                    />
-                    <Box
-                      justifyContent="center"
-                      display="flex"
-                      flexDir="column">
-                      <Text mb="4px">
-                        {userNameFactory(
-                          n.editors?.length ? n.editors[0] : undefined,
-                        )}
-                      </Text>
-                      <Text color="gray">
-                        {dateTimeFormatterFromJSDDate({
-                          dateTime: new Date(n.createdAt),
-                        })}
-                      </Text>
+                  flexDir="column"
+                  alignItems="center">
+                  <Box
+                    mb="8px"
+                    flexDir="row"
+                    display="flex"
+                    alignItems="center"
+                    w="100%"
+                    justifyContent="space-between">
+                    <Box flexDir="row" display="flex">
+                      <Avatar
+                        src={
+                          n.editors?.length ? n.editors[0].avatarUrl : undefined
+                        }
+                        size="md"
+                        mr="4px"
+                      />
+                      <Box
+                        justifyContent="center"
+                        display="flex"
+                        flexDir="column">
+                        <Text mb="4px">
+                          {userNameFactory(
+                            n.editors?.length ? n.editors[0] : undefined,
+                          )}
+                        </Text>
+                        <Text color="gray">
+                          {dateTimeFormatterFromJSDDate({
+                            dateTime: new Date(n.createdAt),
+                          })}
+                        </Text>
+                      </Box>
                     </Box>
+                    <Menu>
+                      <MenuButton
+                        as={IconButton}
+                        aria-label="Options"
+                        icon={<FiMenu />}
+                        variant="outline"
+                      />
+                      <MenuList>
+                        <MenuItem onClick={() => onClickEdit(n)}>編集</MenuItem>
+                        <MenuItem onClick={() => onClickDelete(n)}>
+                          削除
+                        </MenuItem>
+                      </MenuList>
+                    </Menu>{' '}
                   </Box>
-                  <Menu>
-                    <MenuButton
-                      as={IconButton}
-                      aria-label="Options"
-                      icon={<FiMenu />}
-                      variant="outline"
-                    />
-                    <MenuList>
-                      <MenuItem onClick={() => onClickEdit(n)}>編集</MenuItem>
-                      <MenuItem onClick={() => onClickDelete(n)}>削除</MenuItem>
-                    </MenuList>
-                  </Menu>{' '}
+                  <SimpleGrid spacing="4px" columns={3} w="100%">
+                    {n.images?.map((i) => (
+                      <Link onClick={() => setSelectedImage(i)} key={i.id}>
+                        <Image src={i.imageURL} alt="関連画像" />
+                      </Link>
+                    ))}
+                  </SimpleGrid>
+                  <Text alignSelf="flex-start">{n.content}</Text>
                 </Box>
-                <SimpleGrid spacing="4px" columns={3} w="100%">
-                  {n.images?.map((i) => (
-                    <Image key={i.id} src={i.imageURL} alt="関連画像" />
-                  ))}
-                </SimpleGrid>
-                <Text alignSelf="flex-start">{n.content}</Text>
-              </Box>
-            ))
-          ) : (
-            <Box>
-              <Box mb="8px">
-                <Box
-                  flexDir="row"
-                  justifyContent="space-between"
-                  display="flex">
-                  <Button
-                    size="sm"
+              ))
+            ) : (
+              <Box>
+                <Box mb="8px">
+                  <Box
                     flexDir="row"
-                    onClick={onClickBackButton}
-                    mb="8px"
-                    alignItems="center">
-                    <AiOutlineLeft size={24} style={{ display: 'inline' }} />
-                    <Text display="inline">一覧へ戻る</Text>
-                  </Button>
-                  <Button
-                    size="sm"
-                    flexDir="row"
-                    onClick={() => imageUploaderRef.current?.click()}
-                    mb="8px"
-                    colorScheme="green"
-                    alignItems="center">
-                    <Text display="inline">写真を追加</Text>
-                  </Button>
-                  <input
-                    multiple
-                    ref={imageUploaderRef}
-                    accept="image/*"
-                    type="file"
-                    hidden
-                    name="imageUploadToAlbum"
-                    onChange={() => {
-                      const files = imageUploaderRef.current?.files;
-                      const fileArr: File[] = [];
-                      if (!files) {
-                        return;
-                      }
-                      for (let i = 0; i < files.length; i++) {
-                        const renamedFile = new File(
-                          [files[i]],
-                          files[i].name,
-                          {
-                            type: files[i].type,
-                            lastModified: files[i].lastModified,
-                          },
-                        );
-                        fileArr.push(renamedFile);
-                      }
-                      onUploadImage(fileArr);
-                    }}
+                    justifyContent="space-between"
+                    display="flex">
+                    <Button
+                      size="sm"
+                      flexDir="row"
+                      onClick={onClickBackButton}
+                      mb="8px"
+                      alignItems="center">
+                      <AiOutlineLeft size={24} style={{ display: 'inline' }} />
+                      <Text display="inline">一覧へ戻る</Text>
+                    </Button>
+                    <Button
+                      size="sm"
+                      flexDir="row"
+                      onClick={() => imageUploaderRef.current?.click()}
+                      mb="8px"
+                      colorScheme="green"
+                      alignItems="center">
+                      <Text display="inline">写真を追加</Text>
+                    </Button>
+                    <input
+                      multiple
+                      ref={imageUploaderRef}
+                      accept="image/*"
+                      type="file"
+                      hidden
+                      name="imageUploadToAlbum"
+                      onChange={() => {
+                        const files = imageUploaderRef.current?.files;
+                        const fileArr: File[] = [];
+                        if (!files) {
+                          return;
+                        }
+                        for (let i = 0; i < files.length; i++) {
+                          const renamedFile = new File(
+                            [files[i]],
+                            files[i].name,
+                            {
+                              type: files[i].type,
+                              lastModified: files[i].lastModified,
+                            },
+                          );
+                          fileArr.push(renamedFile);
+                        }
+                        onUploadImage(fileArr);
+                      }}
+                    />
+                  </Box>
+                  <SimpleGrid mb="8px" spacing="4px" columns={3} w="100%">
+                    {values.images?.map((i) => (
+                      <Link key={i.id} onClick={() => setSelectedImage(i)}>
+                        <Image src={i.imageURL} alt="関連画像" />
+                      </Link>
+                    ))}
+                  </SimpleGrid>
+                  <Textarea
+                    value={values.content}
+                    name="content"
+                    onChange={handleChange}
+                    placeholder="今なにしてる？"
+                    bg="white"
+                    minH={'300px'}
                   />
                 </Box>
-                <SimpleGrid mb="8px" spacing="4px" columns={3} w="100%">
-                  {values.images?.map((i) => (
-                    <Image key={i.id} src={i.imageURL} alt="関連画像" />
-                  ))}
-                </SimpleGrid>
-                <Textarea
-                  value={values.content}
-                  name="content"
-                  onChange={handleChange}
-                  placeholder="今なにしてる？"
-                  bg="white"
-                  minH={'300px'}
-                />
+                <Box display="flex" justifyContent="center" alignItems="center">
+                  <Button colorScheme="pink" onClick={() => handleSubmit()}>
+                    更新
+                  </Button>
+                </Box>
               </Box>
-              <Box display="flex" justifyContent="center" alignItems="center">
-                <Button colorScheme="pink" onClick={() => handleSubmit()}>
-                  更新
-                </Button>
-              </Box>
-            </Box>
-          )}
-        </ModalBody>
-      </ModalContent>
-    </Modal>
+            )}
+          </ModalBody>
+        </ModalContent>
+      </Modal>
+    </>
   );
 };
 
