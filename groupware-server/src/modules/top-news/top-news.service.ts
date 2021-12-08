ad6705679ee1608e5ca9994ac80040c4a6e73133
Kodai Kabasawa <kabaaa1126@gmail.com>
2021-12-08 15:03:49 +0900

    add endpoint related to top news CU-1wv410b[completed] CU-1wv40vh[completed] CU-1wv40w1[completed] CU-1wv40wn[completed]

---
+++ groupware-server/src/modules/top-news/top-news.service.ts
@@ -0,0 +1,37 @@
+import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { TopNews } from 'src/entities/topNews.entity';
+import { Repository } from 'typeorm';
+import { GetTopNewsQuery, GetTopNewsResult } from './top-news.controller';
+
+@Injectable()
+export class TopNewsService {
+  constructor(
+    @InjectRepository(TopNews)
+    private readonly newsRepository: Repository<TopNews>,
+  ) {}
+
+  async getTopNews(query: GetTopNewsQuery): Promise<GetTopNewsResult> {
+    const { page = 1 } = query;
+    const limit = 20;
+    const offset = (Number(page) - 1) * limit;
+    const [news, count] = await this.newsRepository
+      .createQueryBuilder('news')
+      .select()
+      .skip(offset)
+      .take(limit)
+      .orderBy('events.startAt', 'ASC')
+      .getManyAndCount();
+    const pageCount =
+      count % limit === 0 ? count / limit : Math.floor(count / limit) + 1;
+    return { pageCount, news };
+  }
+
+  async saveNews(news: Partial<TopNews>): Promise<TopNews> {
+    return await this.newsRepository.save(news);
+  }
+
+  async deleteNews(newsId: number) {
+    await this.newsRepository.delete(newsId);
+  }
+}
