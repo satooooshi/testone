d8165e4f7c8f711f46cec9f1d613f38cb8f86244
Kodai Kabasawa <kabaaa1126@gmail.com>
2021-12-10 00:13:27 +0900

    remove unneeded device token log

--- GroupwareMobile/src/App.tsx
+++ GroupwareMobile/src/App.tsx
@@ -3,32 +3,11 @@ import Navigator from './navigator';
 import {AuthenticateProvider} from './contexts/useAuthenticate';
 import {QueryClientProvider, QueryClient} from 'react-query';
 import 'react-native-gesture-handler';
-import {requestIOSMsgPermission} from './utils/permission/requestIOSMsgPermisson';
-import messaging from '@react-native-firebase/messaging';
 import RNBootSplash from 'react-native-bootsplash';
 import WebEngine from './components/WebEngine';
 
 const App = () => {
   const queryClient = new QueryClient();
-
-  useEffect(() => {
-    const handleMessaging = async () => {
-      await requestIOSMsgPermission();
-      // const token =
-      //   Platform.OS === 'android'
-      //     ? await messaging().getToken()
-      //     : await messaging().getAPNSToken();
-      const token = await messaging().getToken();
-      console.log('token:', token);
-
-      // Listen to whether the token changes
-      return messaging().onTokenRefresh(tokenChanged => {
-        // saveTokenToDatabase(token);
-        console.log('token changed: ', tokenChanged);
-      });
-    };
-    handleMessaging();
-  }, []);
 
   useEffect(() => {
     (async () => {
