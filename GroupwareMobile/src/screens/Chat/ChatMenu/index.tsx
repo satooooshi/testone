1f525e95e7024e903f625cf0477d6ab5157bac93
Kodai Kabasawa <kabaaa1126@gmail.com>
2021-12-09 12:13:27 +0900

    add feature to leave chat room in mobile

--- GroupwareMobile/src/screens/Chat/ChatMenu/index.tsx
+++ GroupwareMobile/src/screens/Chat/ChatMenu/index.tsx
@@ -1,9 +1,11 @@
 import {useNavigation, useRoute} from '@react-navigation/native';
 import React from 'react';
+import {Alert} from 'react-native';
 import {Icon} from 'react-native-magnus';
 import ChatMenuRow from '../../../components/chat/ChatMenuRow';
 import HeaderWithTextButton from '../../../components/Header';
 import WholeContainer from '../../../components/WholeContainer';
+import {useAPILeaveChatRoom} from '../../../hooks/api/chat/useAPILeaveChatRoomURL';
 import {
   ChatMenuNavigationProps,
   ChatMenuRouteProps,
@@ -13,6 +15,14 @@ const ChatMenu: React.FC = () => {
   const route = useRoute<ChatMenuRouteProps>();
   const navigation = useNavigation<ChatMenuNavigationProps>();
   const {room} = route.params;
+  const {mutate: leaveChatGroup} = useAPILeaveChatRoom({
+    onSuccess: () => {
+      navigation.navigate('ChatStack', {
+        screen: 'RoomList',
+        params: {needRefetch: true},
+      });
+    },
+  });
 
   return (
     <WholeContainer>
@@ -53,6 +63,33 @@ const ChatMenu: React.FC = () => {
             screen: 'ChatAlbums',
             params: {room},
           })
+        }
+      />
+      <ChatMenuRow
+        name="退室"
+        icon={
+          <Icon
+            name="ios-arrow-undo-outline"
+            fontSize={20}
+            fontFamily="Ionicons"
+            mr={'lg'}
+            color="black"
+          />
+        }
+        onPress={() =>
+          Alert.alert('退室してよろしいですか？', undefined, [
+            {
+              text: 'キャンセル',
+              style: 'cancel',
+            },
+            {
+              text: '退室する',
+              style: 'destructive',
+              onPress: () => {
+                leaveChatGroup(room);
+              },
+            },
+          ])
         }
       />
     </WholeContainer>
