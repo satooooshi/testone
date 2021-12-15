c50a21731a0d59a62227988c7e37a208a6d3f0b0
Kodai Kabasawa <kabaaa1126@gmail.com>
2021-12-15 22:08:34 +0900

    display react-native-image-crop-picker error CU-1yzm20y[closed]

--- GroupwareMobile/src/utils/cropImage/uploadImageFromGallery.ts
+++ GroupwareMobile/src/utils/cropImage/uploadImageFromGallery.ts
@@ -1,5 +1,9 @@
 import {Alert, Platform} from 'react-native';
-import ImagePicker, {Image, Options} from 'react-native-image-crop-picker';
+import ImagePicker, {
+  Image,
+  Options,
+  PickerErrorCode,
+} from 'react-native-image-crop-picker';
 
 export const uploadImageFromGallery = async (
   options: Options = {
@@ -14,9 +18,61 @@ export const uploadImageFromGallery = async (
     const formData = imagePickerResponseToFormData(photo);
     return {formData, mime};
   } catch (err) {
-    Alert.alert(
-      'ギャラリーの表示に失敗しました。\n端末の設定からアプリにアクセスの許可がされているかご確認ください。',
-    );
+    const code = (err as {code: PickerErrorCode})?.code;
+    switch (code) {
+      case 'E_PICKER_CANCELLED':
+        break;
+      case 'E_NO_IMAGE_DATA_FOUND':
+        Alert.alert(
+          'ギャラリーの表示に失敗しました。\n端末の設定からアプリにアクセスの許可がされているかご確認ください。',
+        );
+        break;
+      case 'E_NO_LIBRARY_PERMISSION':
+        Alert.alert(
+          'ギャラリーの表示に失敗しました。\n端末の設定からアプリにアクセスの許可がされているかご確認ください。',
+        );
+        break;
+      case 'E_NO_CAMERA_PERMISSION':
+        Alert.alert(
+          'カメラの表示に失敗しました。\n端末の設定からアプリにアクセスの許可がされているかご確認ください。',
+        );
+        break;
+      case 'E_PICKER_CANNOT_RUN_CAMERA_ON_SIMULATOR':
+        Alert.alert('シミュレーターでカメラの使用はできません。');
+        break;
+      case 'E_CROPPER_IMAGE_NOT_FOUND':
+        Alert.alert('エラーが発生しました。');
+        break;
+      case 'E_CANNOT_SAVE_IMAGE':
+        Alert.alert('エラーが発生しました。');
+        break;
+      case 'E_ACTIVITY_DOES_NOT_EXIST':
+        Alert.alert('エラーが発生しました。');
+        break;
+      case 'E_CALLBACK_ERROR':
+        Alert.alert('エラーが発生しました。');
+        break;
+      case 'E_FAILED_TO_SHOW_PICKER':
+        Alert.alert(
+          'ギャラリーの表示に失敗しました。\n端末の設定からアプリにアクセスの許可がされているかご確認ください。',
+        );
+        break;
+      case 'E_FAILED_TO_OPEN_CAMERA':
+        Alert.alert(
+          'ギャラリーの表示に失敗しました。\n端末の設定からアプリにアクセスの許可がされているかご確認ください。',
+        );
+        break;
+      case 'E_CAMERA_IS_NOT_AVAILABLE':
+        Alert.alert(
+          'ギャラリーの表示に失敗しました。\n端末の設定からアプリにアクセスの許可がされているかご確認ください。',
+        );
+        break;
+      case 'E_CANNOT_LAUNCH_CAMERA':
+        Alert.alert(
+          'カメラの表示に失敗しました。\n端末の設定からアプリにアクセスの許可がされているかご確認ください。',
+        );
+        break;
+    }
   }
 };
 
