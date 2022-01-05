dec5dd1251625a3fdca1281efb06c77877c70a88
Kodai Kabasawa <kabaaa1126@gmail.com>
2022-01-05 14:07:56 +0900

    remove unsed emoji-picker

--- groupware-frontend/src/pages/chat/[id].tsx
+++ groupware-frontend/src/pages/chat/[id].tsx
@@ -1,19 +1,9 @@
 import { SidebarScreenName } from '@/components/layout/Sidebar';
 import { MenuValue, useModalReducer } from '@/hooks/chat/useModalReducer';
 import React, { useState } from 'react';
-import { ChatGroup, ChatMessage } from 'src/types';
+import { ChatGroup } from 'src/types';
 import CreateChatGroupModal from '@/components/chat/CreateChatGroupModal';
-import {
-  useMediaQuery,
-  Box,
-  Modal,
-  ModalHeader,
-  ModalCloseButton,
-  ModalBody,
-  ModalContent,
-  ModalOverlay,
-  useToast,
-} from '@chakra-ui/react';
+import { useMediaQuery, Box, useToast } from '@chakra-ui/react';
 import LayoutWithTab from '@/components/layout/LayoutWithTab';
 import { Tab } from 'src/types/header/tab/types';
 import Head from 'next/head';
@@ -24,8 +14,6 @@ import { useHeaderTab } from '@/hooks/headerTab/useHeaderTab';
 import { useAPILeaveChatRoom } from '@/hooks/api/chat/useAPILeaveChatRoomURL';
 import ChatBox from '@/components/chat/ChatBox';
 import 'emoji-mart/css/emoji-mart.css';
-import { Picker } from 'emoji-mart';
-import { useAPISaveReaction } from '@/hooks/api/chat/useAPISaveReaction';
 import RoomList from '@/components/chat/RoomList';
 import { useAPIUpdateChatGroup } from '@/hooks/api/chat/useAPIUpdateChatGroup';
 import { RoomRefetchProvider } from 'src/contexts/chat/useRoomRefetch';
@@ -33,9 +21,6 @@ import { RoomRefetchProvider } from 'src/contexts/chat/useRoomRefetch';
 const ChatDetail = () => {
   const router = useRouter();
   const { id } = router.query as { id: string };
-  const { mutate: saveReaction } = useAPISaveReaction();
-  const [selectedMsgForReaction, setSelectedMsgForReaction] =
-    useState<ChatMessage>();
   const [currentRoom, setCurrentRoom] = useState<ChatGroup>();
   const [
     { editChatGroupModalVisible, createGroupWindow, editMembersModalVisible },
@@ -100,51 +85,6 @@ const ChatDetail = () => {
         <Head>
           <title>ボールド | Chat</title>
         </Head>
-        <Modal
-          isOpen={!!selectedMsgForReaction}
-          onClose={() => setSelectedMsgForReaction(undefined)}>
-          <ModalOverlay />
-          <ModalContent h="90vh" bg={'#f9fafb'}>
-            <ModalHeader>リアクションを選択</ModalHeader>
-            <ModalCloseButton />
-            <ModalBody display="flex" flexDir="row" justifyContent="center">
-              <Picker
-                title=""
-                emoji="point_up"
-                skin={1}
-                defaultSkin={1}
-                // eslint-disable-next-line @typescript-eslint/ban-ts-comment
-                //@ts-ignore for Android suport
-                emojisToShowFilter={(e) => parseFloat(e['added_in']) <= 6}
-                onSelect={(emoji) => {
-                  saveReaction({
-                    // eslint-disable-next-line @typescript-eslint/ban-ts-comment
-                    //@ts-ignore
-                    emoji: emoji?.native,
-                    chatMessage: selectedMsgForReaction,
-                  });
-                  setSelectedMsgForReaction(undefined);
-                }}
-                i18n={{
-                  search: '検索',
-                  categories: {
-                    search: '検索結果',
-                    recent: 'よく使われる絵文字',
-                    people: '顔 & 人',
-                    nature: '動物 & 自然',
-                    foods: '食べ物 & 飲み物',
-                    activity: 'アクティビティ',
-                    places: '旅行 & 場所',
-                    objects: 'オブジェクト',
-                    symbols: '記号',
-                    flags: '旗',
-                    custom: 'カスタム',
-                  },
-                }}
-              />
-            </ModalBody>
-          </ModalContent>
-        </Modal>
 
         <CreateChatGroupModal
           isOpen={createGroupWindow}
