f9bc54e3d6a8ce1a3aac827f347a6fab4ed71b80
k-tkm <kjohlkjou89@gmail.com>
2022-01-31 13:53:57 +0900

    Fix selectedMembers type

--- groupware-frontend/src/components/chat/CreateChatGroupModal/index.tsx
+++ groupware-frontend/src/components/chat/CreateChatGroupModal/index.tsx
@@ -39,7 +39,7 @@ type CreateChatGroupModalProps = {
   isOpen: boolean;
   closeModal: () => void;
   onComplete: () => void;
-  selectedMembers: Partial<User>[];
+  selectedMembers: User[];
 };
 
 const CreateChatGroupModal: React.FC<CreateChatGroupModalProps> = ({
