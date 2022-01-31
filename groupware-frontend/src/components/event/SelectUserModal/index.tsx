e4851c52a1e3ab8f221385d9e13904b9b266a2ea
Kodai Kabasawa <kabaaa1126@gmail.com>
2022-01-31 15:16:16 +0900

    tiny fix wording

--- groupware-frontend/src/components/event/SelectUserModal/index.tsx
+++ groupware-frontend/src/components/event/SelectUserModal/index.tsx
@@ -94,7 +94,7 @@ const SelectUserModal: React.FC<SelectUserModalProps> = ({
       isOpen={isOpen}
       className={selectUserModalStyles.modal}>
       <FormControl className={selectUserModalStyles.user_role_select_wrapper}>
-        <FormLabel htmlFor="search">ユーザーを検索</FormLabel>
+        <FormLabel htmlFor="search">名前で検索</FormLabel>
         <Input
           marginBottom={'8px'}
           onChange={(v) => onChangeHandle(v.target.value)}
