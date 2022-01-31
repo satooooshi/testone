2b4c24221ab274050f184be1b73605dbc5d73c5d
k-tkm <kjohlkjou89@gmail.com>
2022-01-31 13:26:44 +0900

    Fix Chat/index.tsx so that you can also select the room type in the menu.

--- groupware-frontend/src/components/chat/CreateChatGroupModal/index.tsx
+++ groupware-frontend/src/components/chat/CreateChatGroupModal/index.tsx
@@ -39,7 +39,7 @@ type CreateChatGroupModalProps = {
   isOpen: boolean;
   closeModal: () => void;
   onComplete: () => void;
-  selectedMembers: User[];
+  selectedMembers: Partial<User>[];
 };
 
 const CreateChatGroupModal: React.FC<CreateChatGroupModalProps> = ({
