202cc0dfcf4b700b46db9d9bf87d3518b1fff0b9
U ser <nahoshimizu@appurunoMacBook-Pro.local>
2022-02-09 20:23:46 +0900

    Implemented message search hooks

--- GroupwareMobile/src/utils/url/chat.url.ts
+++ GroupwareMobile/src/utils/url/chat.url.ts
@@ -4,6 +4,7 @@ export const getChatGroupListURL = `/${chatURL}/group-list`;
 export const getRoomsByPageURL = `/${chatURL}/v2/rooms`;
 export const getRoomDetailURL = `/${chatURL}/v2/room`;
 export const getChatMessagesURL = `/${chatURL}/get-messages`;
+export const searchMessagesURL = `/${chatURL}/search-messages`;
 export const editMembersURL = (roomId: number) =>
   `/${chatURL}/v2/room/${roomId.toString()}/members`;
 export const getLatestMentionedMessageURL = `/${chatURL}/latest-mentioned`;
