2fd40dc927a5ae0f9b055d4c1189271aa9f22418
k-tkm <kjohlkjou89@gmail.com>
2022-02-16 14:32:07 +0900

    Modify the design of the administrator screen.

--- GroupwareMobile/src/components/common/DropdownOpenerButton/index.tsx
+++ GroupwareMobile/src/components/common/DropdownOpenerButton/index.tsx
@@ -4,11 +4,13 @@ import {Button, Icon} from 'react-native-magnus';
 type DropdownOpenerButtonProps = {
   name: string;
   onPress: () => void;
+  fontSize?: number;
 };
 
 const DropdownOpenerButton: React.FC<DropdownOpenerButtonProps> = ({
   name,
   onPress,
+  fontSize,
 }) => {
   return (
     <Button
@@ -17,6 +19,7 @@ const DropdownOpenerButton: React.FC<DropdownOpenerButtonProps> = ({
       bg="white"
       borderWidth={1}
       borderColor={'#e0e0e0'}
+      fontSize={fontSize}
       p={12}
       color="black"
       onPress={onPress}
