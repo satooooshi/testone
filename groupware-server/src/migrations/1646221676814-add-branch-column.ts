0922e25595b50eda81a1a0301af2589d43e0c298
U ser <nahoshimizu@appurunoMacBook-Pro.local>
2022-03-02 22:00:37 +0900

    Added branch column to users table

---
+++ groupware-server/src/migrations/1646221676814-add-branch-column.ts
@@ -0,0 +1,17 @@
+import { MigrationInterface, QueryRunner } from 'typeorm';
+
+export class addBranchColumn1646221676814 implements MigrationInterface {
+  name = 'addBranchColumn1646221676814';
+
+  public async up(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.query(
+      `ALTER TABLE \`groupware-mysql8\`.\`users\` ADD \`branch\` enum ('tokyo', 'osaka', 'non_set') NOT NULL DEFAULT 'non_set'`,
+    );
+  }
+
+  public async down(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.query(
+      `ALTER TABLE \`groupware-mysql8\`.\`users\` DROP COLUMN \`branch\``,
+    );
+  }
+}
