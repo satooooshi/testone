0b4c016e4b8fae70d993c5b1fa8497123e8d2521
shige <shigebask@gmail.com>
2022-03-11 14:41:59 +0900

    create AttendanceReportRow

---
+++ GroupwareMobile/src/components/attendance/AttendanceReportRow/index.tsx
@@ -0,0 +1,32 @@
+import {useFormik} from 'formik';
+import {DateTime} from 'luxon';
+import React, {useEffect, useRef, useState} from 'react';
+import {Alert, u} from 'react-native';
+import {Button, Div, Text} from 'react-native-magnus';
+import {AttendanceReport} from '../../../types';
+
+type AttendanceReportRowProps = {
+  reportData?: AttendanceReport;
+};
+
+const AttendanceReportRow: React.FC<AttendanceReportRowProps> = ({
+  reportData,
+}) => {
+  return (
+    <>
+      <Div minW={'20%'} justifyContent="center" alignItems="center">
+        <Text fontSize={16}>{reportData?.targetDate}</Text>
+      </Div>
+      <Div minW={'30%'} justifyContent="center" alignItems="center">
+        <Text>{reportData?.category}</Text>
+      </Div>
+      <Div minW={'30%'} justifyContent="center" alignItems="center">
+        <Text>{reportData?.createdAt}</Text>
+      </Div>
+      <Div minW={'20%'} alignItems="center">
+        <Text>{reportData?.verifiedAt}</Text>
+      </Div>
+    </>
+  );
+};
+export default AttendanceReportRow;
