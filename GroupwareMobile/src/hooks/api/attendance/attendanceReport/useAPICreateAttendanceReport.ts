b9efb7f6516b0452914fb1cb6c393ff0742820ac
shige <shigebask@gmail.com>
2022-03-11 14:33:08 +0900

    tiny fix

---
+++ GroupwareMobile/src/hooks/api/attendance/attendanceReport/useAPICreateAttendanceReport.ts
@@ -0,0 +1,29 @@
+import {AxiosError} from 'axios';
+import {useMutation, UseMutationOptions} from 'react-query';
+import {AttendanceReport} from '../../../../types';
+import {axiosInstance} from '../../../../utils/url';
+import {attendanceReportURL} from '../../../../utils/url/attendance.url';
+
+const createAttendanceReport = async (query: Partial<AttendanceReport>) => {
+  const res = await axiosInstance.post<AttendanceReport>(
+    attendanceReportURL,
+    query,
+  );
+  return res.data;
+};
+
+export const useAPICreateAttendanceReport = (
+  options?: UseMutationOptions<
+    AttendanceReport,
+    AxiosError,
+    Partial<AttendanceReport>,
+    unknown
+  >,
+) => {
+  return useMutation<
+    AttendanceReport,
+    AxiosError,
+    Partial<AttendanceReport>,
+    unknown
+  >(createAttendanceReport, options);
+};
---
+++ GroupwareMobile/src/hooks/api/attendance/attendanceReport/useAPIGetAttendanceReport.ts
@@ -0,0 +1,31 @@
+import {AxiosError} from 'axios';
+import {useQuery, UseQueryOptions} from 'react-query';
+import {AttendanceReport} from '../../../../types';
+import {axiosInstance} from '../../../../utils/url';
+import {attendanceReportURL} from '../../../../utils/url/attendance.url';
+
+export interface GetAttendanceReportQuery {
+  from_date: string;
+  to_date: string;
+}
+
+const getAttendanceReport = async (
+  query: GetAttendanceReportQuery,
+): Promise<AttendanceReport[]> => {
+  const {from_date: fromDate, to_date: toDate} = query;
+  const response = await axiosInstance.get<AttendanceReport[]>(
+    attendanceReportURL + `?from_date=${fromDate}&to_date=${toDate}`,
+  );
+  return response.data;
+};
+
+export const useAPIGetAttendaceReport = (
+  query: GetAttendanceReportQuery,
+  options?: UseQueryOptions<AttendanceReport[], AxiosError>,
+) => {
+  return useQuery<AttendanceReport[], AxiosError>(
+    ['getAttendanceReport', query],
+    () => getAttendanceReport(query),
+    options,
+  );
+};
