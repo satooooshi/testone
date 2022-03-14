86d35298c3d1d9b9a9c07befb68fcedacbdfc6ef
shige <shigebask@gmail.com>
2022-03-14 15:54:49 +0900

    create useAPIGetAllUnverifiedAttendanceReport in mobile

---
+++ GroupwareMobile/src/hooks/api/attendance/attendanceReport/useAPIGetAllUnverifiedAttendanceReport.ts
@@ -0,0 +1,31 @@
+import {AxiosError} from 'axios';
+import {useQuery, UseQueryOptions} from 'react-query';
+import {AttendanceRepo} from '../../../../types';
+import {axiosInstance} from '../../../../utils/url';
+import {attendanceReportURL} from '../../../../utils/url/attendance.url';
+
+export interface GetAttendanceReportQuery {
+  from_date: string;
+  to_date: string;
+}
+
+const getAllUnverifiedAttendanceReport = async (
+  query: GetAttendanceReportQuery,
+): Promise<AttendanceRepo[]> => {
+  const {from_date: fromDate, to_date: toDate} = query;
+  const response = await axiosInstance.get<AttendanceRepo[]>(
+    attendanceReportURL + `?from_date=${fromDate}&to_date=${toDate}`,
+  );
+  return response.data;
+};
+
+export const useAPIGetAllUnverifiedAttendanceReport = (
+  query: GetAttendanceReportQuery,
+  options?: UseQueryOptions<AttendanceRepo[], AxiosError>,
+) => {
+  return useQuery<AttendanceRepo[], AxiosError>(
+    ['getAllUnverifiedAttendanceReport', query],
+    () => getAllUnverifiedAttendanceReport(query),
+    options,
+  );
+};
