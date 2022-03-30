e2fd18a288c600fc129c207a4a34c74f0a8078f1
k-tkm <kjohlkjou89@gmail.com>
2022-03-30 20:27:46 +0900

    tiny fix

--- GroupwareMobile/index.js
+++ GroupwareMobile/index.js
@@ -2,7 +2,7 @@
  * @format
  */
 import React from 'react';
-import {AppRegistry, Platform} from 'react-native';
+import {AppRegistry} from 'react-native';
 import App from './src/App';
 import {name as appName} from './app.json';
 import 'intl';
