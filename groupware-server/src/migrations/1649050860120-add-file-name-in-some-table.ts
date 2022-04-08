78090d051cfc7d92cad1843fef5a0e126a63902c
k-tkm <kjohlkjou89@gmail.com>
2022-04-08 13:14:32 +0900

    fix query in add-file-name-in-some.ts

--- groupware-server/src/migrations/1649050860120-add-file-name-in-some-table.ts
+++ groupware-server/src/migrations/1649050860120-add-file-name-in-some-table.ts
@@ -5,9 +5,6 @@ export class addFileNameInSomeTable1649050860120 implements MigrationInterface {
 
   public async up(queryRunner: QueryRunner): Promise<void> {
     await queryRunner.query(
-      `ALTER TABLE chat_messages CHANGE call_time file_name varchar(255) NOT NULL DEFAULT ''`,
-    );
-    await queryRunner.query(
       `ALTER TABLE chat_album_images ADD file_name varchar(2083) NOT NULL DEFAULT ''`,
     );
     await queryRunner.query(
@@ -29,9 +26,6 @@ export class addFileNameInSomeTable1649050860120 implements MigrationInterface {
     );
     await queryRunner.query(
       `ALTER TABLE chat_album_images DROP COLUMN file_name`,
-    );
-    await queryRunner.query(
-      `ALTER TABLE chat_messages CHANGE file_name call_time varchar(255) NOT NULL DEFAULT ''`,
     );
   }
 }
