01d7edfc3868532e3c3f8ed2fce728e5b04788c9
k-tkm <kjohlkjou89@gmail.com>
2022-04-15 12:59:28 +0900

    fix query in add-sticker-in-chat-message-type

--- groupware-server/src/migrations/1649303668434-add-sticker-in-chat-message-type.ts
+++ groupware-server/src/migrations/1649303668434-add-sticker-in-chat-message-type.ts
@@ -7,13 +7,13 @@ export class addStickerInChatMessageType1649303668434
 
   public async up(queryRunner: QueryRunner): Promise<void> {
     await queryRunner.query(
-      `ALTER TABLE chat_messages CHANGE type type enum ('video', 'image', 'text', 'system_text', 'other_file', 'sticker') NOT NULL DEFAULT 'text'`,
+      `ALTER TABLE chat_messages CHANGE type type enum ('video', 'image', 'text', 'call', 'system_text', 'other_file', 'sticker') NOT NULL DEFAULT 'text'`,
     );
   }
 
   public async down(queryRunner: QueryRunner): Promise<void> {
     await queryRunner.query(
-      `ALTER TABLE chat_messages CHANGE type type enum ('video', 'image', 'text', 'system_text', 'other_file') NOT NULL DEFAULT 'text'`,
+      `ALTER TABLE chat_messages CHANGE type type enum ('video', 'image', 'text', 'call', 'system_text', 'other_file') NOT NULL DEFAULT 'text'`,
     );
   }
 }
