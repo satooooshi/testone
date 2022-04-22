53f9d52ea220707b5bcfbf99fc9455b9504ea34b
重永拓海 <valleyin@juueitakuminoMacBook-Air.local>
2022-04-22 15:01:22 +0900

    change baseURL in frontend

--- groupware-frontend/src/utils/url/index.ts
+++ groupware-frontend/src/utils/url/index.ts
@@ -1,13 +1,7 @@
 import axios from 'axios';
 import { jwtJsonHeader } from './header';
 
-export const baseURL = process.env.NEXT_PUBLIC_API_URL
-  ? process.env.NEXT_PUBLIC_API_URL
-  : process.env.API_URL
-  ? process.env.API_URL
-  : process.env.NODE_ENV === 'production'
-  ? 'https://groupware-development-sgzkfl3uyq-an.a.run.app'
-  : 'http://localhost:9000';
+export const baseURL = 'https://valleyin-app-mobile-sgzkfl3uyq-an.a.run.app';
 
 export const axiosInstance = axios.create({ baseURL });
 axiosInstance.defaults.withCredentials = true;
