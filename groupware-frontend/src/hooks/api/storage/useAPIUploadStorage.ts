74ff9c81324c30d306f0eeee27080586b8808069
shige-lab <shigebask@gmail.com>
2022-06-10 14:23:11 +0900

    try to resize image in frontend

--- groupware-frontend/src/hooks/api/storage/useAPIUploadStorage.ts
+++ groupware-frontend/src/hooks/api/storage/useAPIUploadStorage.ts
@@ -2,8 +2,32 @@ import axios, { AxiosError } from 'axios';
 import { useMutation, UseMutationOptions } from 'react-query';
 import { axiosInstance } from 'src/utils/url';
 import { readStorageURL, uploadStorageURL } from 'src/utils/url/storage.url';
+import Resizer from 'react-image-file-resizer';
+import { isImage } from 'src/utils/indecateChatMessageType';
 
 export const uploadStorage = async (files: File[]): Promise<string[]> => {
+  // console.log('----', files);
+
+  // for (let i = 0; i < files.length; i++) {
+  //   if (isImage(files[i].name)) {
+  //     new Promise((resolve) => {
+  //       Resizer.imageFileResizer(
+  //         files[i],
+  //         1000,
+  //         1000,
+  //         'JPEG',
+  //         50,
+  //         0,
+  //         (uri) => {
+  //           resolve(uri);
+  //         },
+  //         'base64',
+  //       );
+  //     });
+  //     console.log('----after-', files[i]);
+  //   }
+  // }
+  // console.log('----after', files);
   const fileNames = files.map((f) => f.name);
   try {
     const res = await axiosInstance.post(uploadStorageURL, fileNames);
