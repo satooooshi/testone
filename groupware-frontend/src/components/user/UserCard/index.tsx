f0cf5b8e85ad3340f49c88b01c0e0048063b4368
osorosia <64416398+osorosia@users.noreply.github.com>
2022-06-16 05:07:57 +0900

    change a click range of link

--- groupware-frontend/src/components/user/UserCard/index.tsx
+++ groupware-frontend/src/components/user/UserCard/index.tsx
@@ -1,7 +1,7 @@
 import React, { useMemo } from 'react';
 import userCardStyles from '@/styles/components/UserCard.module.scss';
 import { Tag, TagType, User } from 'src/types';
-import { Avatar, Button } from '@chakra-ui/react';
+import { Avatar, Box, Button } from '@chakra-ui/react';
 import Link from 'next/link';
 import UserPointCounter from './UserPointCounter';
 
@@ -52,139 +52,139 @@ const UserCard: React.FC<UserCardProps> = ({ user, onClickTag, duration }) => {
     return groupByTagType(user.tags || []);
   }, [user.tags]);
   return (
-    <Link href={`/account/${user.id}`} passHref>
-      <a className={userCardStyles.wrapper}>
-        <div className={userCardStyles.top}>
-          <Avatar
-            size="xl"
-            src={user.avatarUrl}
-            className={userCardStyles.avatar}
+    <Box className={userCardStyles.wrapper}>
+      <div className={userCardStyles.top}>
+        <Avatar
+          size="xl"
+          src={user.avatarUrl}
+          className={userCardStyles.avatar}
+        />
+        <div className={userCardStyles.user_info_text}>
+          <p
+            className={
+              userCardStyles.name
+            }>{`${user.lastName} ${user.firstName}`}</p>
+          <p
+            className={
+              userCardStyles.name_kana
+            }>{`${user.lastNameKana} ${user.firstNameKana}`}</p>
+          <div className={userCardStyles.introduce_text_wrapper}>
+            <p className={userCardStyles.introduce_text}>{`${
+              user.introduceOther || '自己紹介未入力'
+            }`}</p>
+          </div>
+          <UserPointCounter
+            label="event"
+            count={user.eventCount || 0}
+            duration={duration}
           />
-          <div className={userCardStyles.user_info_text}>
-            <p
-              className={
-                userCardStyles.name
-              }>{`${user.lastName} ${user.firstName}`}</p>
-            <p
-              className={
-                userCardStyles.name_kana
-              }>{`${user.lastNameKana} ${user.firstNameKana}`}</p>
-            <div className={userCardStyles.introduce_text_wrapper}>
-              <p className={userCardStyles.introduce_text}>{`${
-                user.introduceOther || '自己紹介未入力'
-              }`}</p>
-            </div>
-            <UserPointCounter
-              label="event"
-              count={user.eventCount || 0}
-              duration={duration}
-            />
-            <UserPointCounter
-              label="question"
-              count={user.questionCount || 0}
-              duration={duration}
-            />
-            <UserPointCounter
-              label="answer"
-              count={user.answerCount || 0}
-              duration={duration}
-            />
-            <UserPointCounter
-              label="knowledge"
-              count={user.knowledgeCount || 0}
-              duration={duration}
-            />
-          </div>
+          <UserPointCounter
+            label="question"
+            count={user.questionCount || 0}
+            duration={duration}
+          />
+          <UserPointCounter
+            label="answer"
+            count={user.answerCount || 0}
+            duration={duration}
+          />
+          <UserPointCounter
+            label="knowledge"
+            count={user.knowledgeCount || 0}
+            duration={duration}
+          />
         </div>
+      </div>
 
-        <div className={userCardStyles.bottom}>
-          <div className={userCardStyles.tags_with_label_wrapper}>
-            <p className={userCardStyles.tags_label}>技術:</p>
-            <div className={userCardStyles.tags_wrapper}>
-              {groupedTags[TagType.TECH].length ? (
-                groupedTags[TagType.TECH].map((t) => (
-                  <TagLink
-                    key={t.id}
-                    tag={t}
-                    onClickTag={onClickTag}
-                    tagColor="teal"
-                  />
-                ))
-              ) : (
-                <Button size="xs" colorScheme="teal">
-                  未設定
-                </Button>
-              )}
-            </div>
+      <div className={userCardStyles.bottom}>
+        <div className={userCardStyles.tags_with_label_wrapper}>
+          <p className={userCardStyles.tags_label}>技術:</p>
+          <div className={userCardStyles.tags_wrapper}>
+            {groupedTags[TagType.TECH].length ? (
+              groupedTags[TagType.TECH].map((t) => (
+                <TagLink
+                  key={t.id}
+                  tag={t}
+                  onClickTag={onClickTag}
+                  tagColor="teal"
+                />
+              ))
+            ) : (
+              <Button size="xs" colorScheme="teal">
+                未設定
+              </Button>
+            )}
           </div>
-          <div className={userCardStyles.tags_with_label_wrapper}>
-            <p className={userCardStyles.tags_label}>資格:</p>
-            <div className={userCardStyles.tags_wrapper}>
-              {groupedTags[TagType.QUALIFICATION].length ? (
-                groupedTags[TagType.QUALIFICATION].map((t) => (
-                  <TagLink
-                    key={t.id}
-                    tag={t}
-                    onClickTag={onClickTag}
-                    tagColor="blue"
-                  />
-                ))
-              ) : (
-                <Button size="xs" height="28px" colorScheme="blue">
-                  未設定
-                </Button>
-              )}
-            </div>
-          </div>
-          <div className={userCardStyles.tags_with_label_wrapper}>
-            <p className={userCardStyles.tags_label}>部活動:</p>
-            <div className={userCardStyles.tags_wrapper}>
-              {groupedTags[TagType.CLUB].length ? (
-                groupedTags[TagType.CLUB].map((t) => (
-                  <TagLink
-                    key={t.id}
-                    tag={t}
-                    onClickTag={onClickTag}
-                    tagColor="green"
-                  />
-                ))
-              ) : (
-                <Button size="xs" height="28px" colorScheme="green">
-                  未設定
-                </Button>
-              )}
-            </div>
-          </div>
-          <div className={userCardStyles.tags_with_label_wrapper}>
-            <p className={userCardStyles.tags_label}>趣味:</p>
-            <div className={userCardStyles.tags_wrapper}>
-              {groupedTags[TagType.HOBBY].length ? (
-                groupedTags[TagType.HOBBY].map((t) => (
-                  <TagLink
-                    key={t.id}
-                    tag={t}
-                    onClickTag={onClickTag}
-                    tagColor="pink"
-                  />
-                ))
-              ) : (
-                <Button size="xs" height="28px" colorScheme="pink">
-                  未設定
-                </Button>
-              )}
-            </div>
-          </div>
-          <Button
-            size="lg"
-            rounded="full"
-            w="50%"
-            fontSize="16px"
-            color="blue.600">
-            プロフィールを見る
-          </Button>
         </div>
-      </a>
-    </Link>
+        <div className={userCardStyles.tags_with_label_wrapper}>
+          <p className={userCardStyles.tags_label}>資格:</p>
+          <div className={userCardStyles.tags_wrapper}>
+            {groupedTags[TagType.QUALIFICATION].length ? (
+              groupedTags[TagType.QUALIFICATION].map((t) => (
+                <TagLink
+                  key={t.id}
+                  tag={t}
+                  onClickTag={onClickTag}
+                  tagColor="blue"
+                />
+              ))
+            ) : (
+              <Button size="xs" height="28px" colorScheme="blue">
+                未設定
+              </Button>
+            )}
+          </div>
+        </div>
+        <div className={userCardStyles.tags_with_label_wrapper}>
+          <p className={userCardStyles.tags_label}>部活動:</p>
+          <div className={userCardStyles.tags_wrapper}>
+            {groupedTags[TagType.CLUB].length ? (
+              groupedTags[TagType.CLUB].map((t) => (
+                <TagLink
+                  key={t.id}
+                  tag={t}
+                  onClickTag={onClickTag}
+                  tagColor="green"
+                />
+              ))
+            ) : (
+              <Button size="xs" height="28px" colorScheme="green">
+                未設定
+              </Button>
+            )}
+          </div>
+        </div>
+        <div className={userCardStyles.tags_with_label_wrapper}>
+          <p className={userCardStyles.tags_label}>趣味:</p>
+          <div className={userCardStyles.tags_wrapper}>
+            {groupedTags[TagType.HOBBY].length ? (
+              groupedTags[TagType.HOBBY].map((t) => (
+                <TagLink
+                  key={t.id}
+                  tag={t}
+                  onClickTag={onClickTag}
+                  tagColor="pink"
+                />
+              ))
+            ) : (
+              <Button size="xs" height="28px" colorScheme="pink">
+                未設定
+              </Button>
+            )}
+          </div>
+        </div>
+        <Button
+          as="a"
+          href={`/account/${user.id}`}
+          size="lg"
+          rounded="full"
+          w="50%"
+          fontSize="16px"
+          color="blue.600">
+          プロフィールを見る
+        </Button>
+      </div>
+    </Box>
   );
 };
 
