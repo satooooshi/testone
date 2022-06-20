9a1faeb743f022ca5bb525a636d922f88986c50f
shige-lab <shigebask@gmail.com>
2022-06-20 20:49:19 +0900

    create TabList

---
+++ groupware-frontend/src/components/common/TabList/index.tsx
@@ -0,0 +1,38 @@
+import { Box, Button } from '@chakra-ui/react';
+import { Tab } from 'src/types/header/tab/types';
+import { hideScrollbarCss } from 'src/utils/chakra/hideScrollBar.css';
+
+type TabListProps = {
+  activeTabName?: string;
+  tabs: Tab[];
+};
+
+const TabList: React.FC<TabListProps> = ({ activeTabName, tabs }) => {
+  return (
+    <Box
+      w={'100%'}
+      //   h="50px"
+      display="flex"
+      flexDir="row"
+      py={3}
+      overflowX="auto"
+      css={hideScrollbarCss}>
+      {tabs?.map((t) =>
+        !t.type ? (
+          <Button
+            minW={`${t.name.length * 20}px`}
+            borderRadius={50}
+            key={t.name}
+            mr={2}
+            colorScheme={t.name === activeTabName ? 'blue' : undefined}
+            bg={t.name !== activeTabName ? 'white' : undefined}
+            onClick={t.onClick}>
+            {t.name}
+          </Button>
+        ) : null,
+      )}
+    </Box>
+  );
+};
+
+export default TabList;
