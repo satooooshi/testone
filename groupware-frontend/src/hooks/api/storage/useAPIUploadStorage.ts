29810af71d2788f5ff3ef49ce25f1f0e5af4f6f9
k-tkm <kjohlkjou89@gmail.com>
2022-06-21 15:41:53 +0900

    Fixed to resize only image file

--- groupware-frontend/src/hooks/api/storage/useAPIUploadStorage.ts
+++ groupware-frontend/src/hooks/api/storage/useAPIUploadStorage.ts
@@ -9,19 +9,21 @@ import Compress from 'compress.js';
 export const uploadStorage = async (files: File[]): Promise<string[]> => {
   const compress = new Compress();
   const resizeImage = async (file: File): Promise<Blob> => {
-    const resizedImage = await compress.compress([file], {
-      size: 2, // the max size in MB, defaults to 2MB
-      quality: 1, // the quality of the image, max is 1,
-      maxWidth: 400, // the max width of the output image, defaults to 1920px
-      maxHeight: 400, // the max height of the output image, defaults to 1920px
-      resize: true, // defaults to true, set false if you do not want to resize the image width and height
-    });
-    const img = resizedImage[0];
-    const base64str = img.data;
-    const imgExt = img.ext;
-    const resizedFiile = Compress.convertBase64ToFile(base64str, imgExt);
-
-    return resizedFiile;
+    if (isImage(file.name)) {
+      const resizedImage = await compress.compress([file], {
+        size: 2, // the max size in MB, defaults to 2MB
+        quality: 1, // the quality of the image, max is 1,
+        maxWidth: 400, // the max width of the output image, defaults to 1920px
+        maxHeight: 400, // the max height of the output image, defaults to 1920px
+        resize: true, // defaults to true, set false if you do not want to resize the image width and height
+      });
+      const img = resizedImage[0];
+      const base64str = img.data;
+      const imgExt = img.ext;
+      const resizedFiile = Compress.convertBase64ToFile(base64str, imgExt);
+      return resizedFiile;
+    }
+    return file;
   };
 
   const fileNames = files.map((f) => f.name);
