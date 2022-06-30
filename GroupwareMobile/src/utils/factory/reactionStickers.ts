58c3c3c42a222dc42d8d8dba2805caedf485dff9
takamuu <mr.takamuu@gmail.com>
2022-06-30 18:34:05 +0900

    Add stamps for Mobile

--- GroupwareMobile/src/utils/factory/reactionStickers.ts
+++ GroupwareMobile/src/utils/factory/reactionStickers.ts
@@ -47,4 +47,72 @@ export const reactionStickers = [
     name: 'willDoSticker',
     src: require('../../../assets/willdo_sticker.png'),
   },
+  {
+    name: 'allRightSticker',
+    src: require('../../../assets/completed_sticker.png'),
+  },
+  {
+    name: 'attachedSticker',
+    src: require('../../../assets/attached_sticker.png'),
+  },
+  {
+    name: 'checkItSticker',
+    src: require('../../../assets/check_it_sticker.png'),
+  },
+  {
+    name: 'closeDeadlineSticker',
+    src: require('../../../assets/close_deadline_sticker.png'),
+  },
+  {
+    name: 'completedSticker',
+    src: require('../../../assets/completed_sticker.png'),
+  },
+  {
+    name: 'confirmationSticker',
+    src: require('../../../assets/confirmation_sticker.png'),
+  },
+  {
+    name: 'congratulationsSticker',
+    src: require('../../../assets/congratulations_sticker.png'),
+  },
+  {
+    name: 'deadlineDaySticker',
+    src: require('../../../assets/deadline_day_sticker.png'),
+  },
+  {
+    name: 'exclamationSticker',
+    src: require('../../../assets/exclamation_sticker.png'),
+  },
+  {
+    name: 'ngSticker',
+    src: require('../../../assets/ng_sticker.png'),
+  },
+  {
+    name: 'notDoneSticker',
+    src: require('../../../assets/not_done_sticker.png'),
+  },
+  {
+    name: 'okSticker',
+    src: require('../../../assets/ok_sticker.png'),
+  },
+  {
+    name: 'okayNowSticker',
+    src: require('../../../assets/okay_now_sticker.png'),
+  },
+  {
+    name: 'osewaninaruSticker',
+    src: require('../../../assets/osewaninaru_sticker.png'),
+  },
+  {
+    name: 'pleaseTakeCareSticker',
+    src: require('../../../assets/please_take_care_sticker.png'),
+  },
+  {
+    name: 'thankYouSticker',
+    src: require('../../../assets/schedule_determined_sticker.png'),
+  },
+  {
+    name: 'scheduleDeterminedSticker',
+    src: require('../../../assets/thank_you_sticker.png'),
+  },
 ];
