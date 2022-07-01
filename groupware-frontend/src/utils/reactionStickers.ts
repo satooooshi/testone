04560709a13b6dd7aae83575a3c2565fc03a97aa
takamuu <mr.takamuu@gmail.com>
2022-07-01 11:24:55 +0900

    6 more stamps for web

--- groupware-frontend/src/utils/reactionStickers.ts
+++ groupware-frontend/src/utils/reactionStickers.ts
@@ -27,6 +27,12 @@ import osewaninaruSticker from '@/public/osewaninaru_sticker.png';
 import pleaseTakeCareSticker from '@/public/please_take_care_sticker.png';
 import scheduleDeterminedSticker from '@/public/schedule_determined_sticker.png';
 import thankYouSticker from '@/public/thank_you_sticker.png';
+import doItSticker from '@/public/do_it_sticker.png';
+import thinkingSticker from '@/public/thinking_sticker.png';
+import iSeeSticker from '@/public/i_see_sticker.png';
+import lostSoulSticker from '@/public/lost_soul_sticker.png';
+import lerningALotSticker from '@/public/lerning_a_lot_sticker.png';
+import callBackSticker from '@/public/call_back_sticker.png';
 
 export const reactionStickers = [
   {
@@ -144,5 +150,29 @@ export const reactionStickers = [
   {
     name: 'scheduleDeterminedSticker',
     src: scheduleDeterminedSticker.src,
+  },
+  {
+    name: 'doItSticker',
+    src: doItSticker.src,
+  },
+  {
+    name: 'thinkingSticker',
+    src: thinkingSticker.src,
+  },
+  {
+    name: 'iSeeSticker',
+    src: iSeeSticker.src,
+  },
+  {
+    name: 'lostSoulSticker',
+    src: lostSoulSticker.src,
+  },
+  {
+    name: 'lerningALotSticker',
+    src: lerningALotSticker.src,
+  },
+  {
+    name: 'callBackSticker',
+    src: callBackSticker.src,
   },
 ];
