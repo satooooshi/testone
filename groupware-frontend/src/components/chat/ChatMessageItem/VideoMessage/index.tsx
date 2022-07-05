3e01d635e31828ea5626d50e9ea0f80b120036c8
k-tkm <kjohlkjou89@gmail.com>
2022-07-05 19:21:53 +0900

    Fixed a bug that prevented videos from opening in safari

--- groupware-frontend/src/components/chat/ChatMessageItem/VideoMessage/index.tsx
+++ groupware-frontend/src/components/chat/ChatMessageItem/VideoMessage/index.tsx
@@ -1,4 +1,4 @@
-import { Box } from '@chakra-ui/react';
+import { Box, Text } from '@chakra-ui/react';
 import React from 'react';
 import { ChatMessage } from 'src/types';
 import ReactPlayer from 'react-player';
@@ -8,21 +8,30 @@ type VideoMessageProps = {
 };
 
 const VideoMessage: React.FC<VideoMessageProps> = ({ message }) => {
+  const userAgent = window.navigator.userAgent;
+  const isSafari = userAgent.includes('Safari');
+  console.error(window.navigator.userAgent);
   return (
     <Box display="flex" maxW="300px" maxH={'300px'}>
-      <ReactPlayer
-        url={message.content}
-        width={300}
-        height={300}
-        config={{
-          file: {
-            attributes: {
-              controlsList: 'nodownload',
+      {isSafari ? (
+        <video controls muted>
+          <source src={message.content} type="video/mp4" />
+        </video>
+      ) : (
+        <ReactPlayer
+          url={message.content}
+          width={300}
+          height={300}
+          config={{
+            file: {
+              attributes: {
+                controlsList: 'nodownload',
+              },
             },
-          },
-        }}
-        controls
-      />
+          }}
+          controls
+        />
+      )}
     </Box>
   );
 };
