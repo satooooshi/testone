eb35317778d0b81579c336758f8d28fe71a76fbf
osorosia <64416398+osorosia@users.noreply.github.com>
2022-07-08 19:42:50 +0900

    change font-size

--- groupware-frontend/src/pages/account/profile/index.tsx
+++ groupware-frontend/src/pages/account/profile/index.tsx
@@ -31,7 +31,6 @@ import ReactCrop from 'react-image-crop';
 import { dataURLToFile } from 'src/utils/dataURLToFile';
 import { useAPIGetProfile } from '@/hooks/api/user/useAPIGetProfile';
 import { useImageCrop } from '@/hooks/crop/useImageCrop';
-import { useHeaderTab } from '@/hooks/headerTab/useHeaderTab';
 import TagModal from '@/components/common/TagModal';
 import { toggleTag } from 'src/utils/toggleTag';
 import { profileSchema } from 'src/utils/validation/schema';
@@ -256,281 +255,303 @@ const Profile = () => {
           onComplete={() => dispatchModal({ type: 'close' })}
         />
       )}
-      <div className={profileStyles.main}>
-        <div className={profileStyles.image_wrapper}>
-          {userInfo.avatarUrl && !selectImageUrl ? (
-            <div
-              {...getEventImageRootProps({
-                className: profileStyles.image_dropzone,
-              })}>
-              <input {...getEventImageInputProps()} />
-              <img
+      <Box className={profileStyles.image_wrapper}>
+        {userInfo.avatarUrl && !selectImageUrl ? (
+          <div
+            {...getEventImageRootProps({
+              className: profileStyles.image_dropzone,
+            })}>
+            <input {...getEventImageInputProps()} />
+            <img
+              className={profileStyles.avatar}
+              src={croppedImageURL ? croppedImageURL : userInfo.avatarUrl}
+              alt="アバター画像"
+            />
+          </div>
+        ) : null}
+        {!userInfo.avatarUrl && !selectImageUrl ? (
+          <div
+            {...getEventImageRootProps({
+              className: profileStyles.image_dropzone,
+            })}>
+            <input {...getEventImageInputProps()} />
+            <div className={profileStyles.next_image_wrapper}>
+              <Image
                 className={profileStyles.avatar}
-                src={croppedImageURL ? croppedImageURL : userInfo.avatarUrl}
+                src={noImage}
                 alt="アバター画像"
               />
             </div>
-          ) : null}
-          {!userInfo.avatarUrl && !selectImageUrl ? (
-            <div
-              {...getEventImageRootProps({
-                className: profileStyles.image_dropzone,
-              })}>
-              <input {...getEventImageInputProps()} />
-              <div className={profileStyles.next_image_wrapper}>
-                <Image
-                  className={profileStyles.avatar}
-                  src={noImage}
-                  alt="アバター画像"
-                />
-              </div>
-            </div>
-          ) : null}
-          {selectImageUrl ? (
-            <ReactCrop
-              keepSelection={true}
-              src={selectImageUrl}
-              crop={crop}
-              onChange={(newCrop) => {
-                dispatchCrop({ type: 'setCrop', value: newCrop });
-              }}
-              onComplete={(c) => {
-                dispatchCrop({
-                  type: 'setCompletedCrop',
-                  value: c,
-                  ref: imgRef.current,
-                });
-              }}
-              onImageLoaded={onLoad}
-              circularCrop={true}
+          </div>
+        ) : null}
+        {selectImageUrl ? (
+          <ReactCrop
+            keepSelection={true}
+            src={selectImageUrl}
+            crop={crop}
+            onChange={(newCrop) => {
+              dispatchCrop({ type: 'setCrop', value: newCrop });
+            }}
+            onComplete={(c) => {
+              dispatchCrop({
+                type: 'setCompletedCrop',
+                value: c,
+                ref: imgRef.current,
+              });
+            }}
+            onImageLoaded={onLoad}
+            circularCrop={true}
+          />
+        ) : null}
+      </Box>
+      <Box className={profileStyles.form_wrapper}>
+        <Stack direction="row">
+          <FormControl className={profileStyles.input_wrapper}>
+            <FormLabel fontWeight={'bold'} fontSize="14px">
+              姓
+            </FormLabel>
+            <Input
+              type="text"
+              name="lastName"
+              placeholder="山田"
+              value={userInfo.lastName}
+              background="white"
+              onChange={handleChange}
             />
-          ) : null}
-        </div>
-        <div className={profileStyles.form_wrapper}>
-          <Stack direction="row">
-            <FormControl className={profileStyles.input_wrapper}>
-              <FormLabel fontWeight={'bold'}>姓</FormLabel>
-              <Input
-                type="text"
-                name="lastName"
-                placeholder="山田"
-                value={userInfo.lastName}
-                background="white"
-                onChange={handleChange}
-              />
-            </FormControl>
-            <FormControl className={profileStyles.input_wrapper}>
-              <FormLabel fontWeight={'bold'}>名</FormLabel>
-              <Input
-                type="text"
-                name="firstName"
-                placeholder="太郎"
-                value={userInfo.firstName}
-                background="white"
-                onChange={handleChange}
-              />
-            </FormControl>
-          </Stack>
-          <Stack direction="row">
-            <FormControl className={profileStyles.input_wrapper}>
-              <FormLabel fontWeight={'bold'}>セイ</FormLabel>
-              <Input
-                type="text"
-                name="lastNameKana"
-                placeholder="ヤマダ"
-                value={userInfo.lastNameKana}
-                background="white"
-                onChange={handleChange}
-              />
-            </FormControl>
+          </FormControl>
+          <FormControl className={profileStyles.input_wrapper}>
+            <FormLabel fontWeight={'bold'} fontSize="14px">
+              名
+            </FormLabel>
+            <Input
+              type="text"
+              name="firstName"
+              placeholder="太郎"
+              value={userInfo.firstName}
+              background="white"
+              onChange={handleChange}
+            />
+          </FormControl>
+        </Stack>
+        <Stack direction="row">
+          <FormControl className={profileStyles.input_wrapper}>
+            <FormLabel fontWeight={'bold'} fontSize="14px">
+              セイ
+            </FormLabel>
+            <Input
+              type="text"
+              name="lastNameKana"
+              placeholder="ヤマダ"
+              value={userInfo.lastNameKana}
+              background="white"
+              onChange={handleChange}
+            />
+          </FormControl>
 
-            <FormControl className={profileStyles.input_wrapper}>
-              <FormLabel fontWeight={'bold'}>メイ</FormLabel>
-              <Input
-                type="text"
-                name="firstNameKana"
-                placeholder="タロウ"
-                value={userInfo.firstNameKana}
-                background="white"
-                onChange={handleChange}
-              />
-            </FormControl>
-          </Stack>
           <FormControl className={profileStyles.input_wrapper}>
-            <FormLabel fontWeight={'bold'}>メールアドレス</FormLabel>
+            <FormLabel fontWeight={'bold'} fontSize="14px">
+              メイ
+            </FormLabel>
             <Input
-              type="email"
-              name="email"
-              placeholder="email@example.com"
-              value={userInfo.email}
+              type="text"
+              name="firstNameKana"
+              placeholder="タロウ"
+              value={userInfo.firstNameKana}
               background="white"
               onChange={handleChange}
             />
-            <Stack spacing={5} direction="row">
-              <Radio
-                bg="white"
-                colorScheme="green"
-                isChecked={userInfo.isEmailPublic}
-                value={'public'}
-                onChange={() =>
-                  setUserInfo((v) => ({ ...v, isEmailPublic: true }))
-                }>
-                公開
-              </Radio>
-              <Radio
-                bg="white"
-                colorScheme="green"
-                isChecked={!userInfo.isEmailPublic}
-                value={'inPublic'}
-                onChange={() =>
-                  setUserInfo((v) => ({ ...v, isEmailPublic: true }))
-                }>
-                非公開
-              </Radio>
-            </Stack>
           </FormControl>
-          <FormControl className={profileStyles.input_wrapper}>
-            <FormLabel fontWeight={'bold'}>電話番号</FormLabel>
-            <Input
-              type="phone"
-              name="phone"
-              placeholder="000-0000-0000"
-              background="white"
-              value={userInfo.phone}
-              onChange={handleChange}
-            />
-            <Stack spacing={5} direction="row">
-              <Radio
-                bg="white"
-                colorScheme="green"
-                isChecked={userInfo.isPhonePublic}
-                value={'public'}
-                onChange={() =>
-                  setUserInfo((v) => ({ ...v, isPhonePublic: true }))
-                }>
-                公開
-              </Radio>
-              <Radio
-                bg="white"
-                colorScheme="green"
-                isChecked={!userInfo.isPhonePublic}
-                value={'unPublic'}
-                onChange={() =>
-                  setUserInfo((v) => ({ ...v, isPhonePublic: false }))
-                }>
-                非公開
-              </Radio>
-            </Stack>
-          </FormControl>
-          <FormControl mb={4}>
-            <FormLabel fontWeight={'bold'}>所属支社</FormLabel>
-            <Select
-              name="branch"
-              value={userInfo.branch}
+        </Stack>
+        <FormControl className={profileStyles.input_wrapper}>
+          <FormLabel fontWeight={'bold'} fontSize="14px">
+            メールアドレス
+          </FormLabel>
+          <Input
+            type="email"
+            name="email"
+            placeholder="email@example.com"
+            value={userInfo.email}
+            background="white"
+            onChange={handleChange}
+          />
+          <Stack spacing={5} direction="row">
+            <Radio
               bg="white"
-              height="10"
-              onChange={handleChange}>
-              <option value={BranchType.NON_SET}>未設定</option>
-              <option value={BranchType.TOKYO}>東京</option>
-              <option value={BranchType.OSAKA}>大阪</option>
-            </Select>
-          </FormControl>
-          <FormControl mb={4}>
-            <FormLabel fontWeight={'bold'}>自己紹介</FormLabel>
-            <Textarea
-              type="text"
-              name="introduceOther"
-              height="10"
-              placeholder="自己紹介を入力してください"
-              value={userInfo.introduceOther}
-              background="white"
-              onChange={handleChange}
-            />
-          </FormControl>
-          <Box mb={2} w={'100%'}>
-            <FormToLinkTag
-              tags={userInfo?.tags || []}
-              tagType={TagType.TECH}
-              toggleTag={toggleSelectedTag}
-              onEditButtonClick={() => dispatchModal({ type: 'openTech' })}
-            />
-          </Box>
-          <FormControl mb={6}>
-            <FormLabel fontWeight={'bold'}>技術の紹介</FormLabel>
-            <Textarea
-              placeholder="技術についての紹介を入力してください"
-              type="text"
-              name="introduceTech"
-              height="10"
-              value={userInfo.introduceTech}
-              background="white"
-              onChange={handleChange}
-            />
-          </FormControl>
-          <Box mb={2} w={'100%'}>
-            <FormToLinkTag
-              tags={userInfo?.tags || []}
-              tagType={TagType.QUALIFICATION}
-              toggleTag={toggleSelectedTag}
-              onEditButtonClick={() =>
-                dispatchModal({ type: 'openQualification' })
-              }
-            />
-          </Box>
-          <FormControl mb={6}>
-            <FormLabel fontWeight={'bold'}>資格の紹介</FormLabel>
-            <Textarea
-              placeholder="資格についての紹介を入力してください"
-              type="text"
-              name="introduceQualification"
-              height="10"
-              value={userInfo.introduceQualification}
-              background="white"
-              onChange={handleChange}
-            />
-          </FormControl>
-          <Box mb={2} w={'100%'}>
-            <FormToLinkTag
-              tags={userInfo?.tags || []}
-              tagType={TagType.CLUB}
-              toggleTag={toggleSelectedTag}
-              onEditButtonClick={() => dispatchModal({ type: 'openClub' })}
-            />
-          </Box>
-          <FormControl mb={6}>
-            <FormLabel fontWeight={'bold'}>部活動の紹介</FormLabel>
-            <Textarea
-              placeholder="部活動についての紹介を入力してください"
-              type="text"
-              name="introduceClub"
-              height="10"
-              value={userInfo.introduceClub}
-              background="white"
-              onChange={handleChange}
-            />
-          </FormControl>
-          <Box mb={2} w={'100%'}>
-            <FormToLinkTag
-              tags={userInfo?.tags || []}
-              tagType={TagType.HOBBY}
-              toggleTag={toggleSelectedTag}
-              onEditButtonClick={() => dispatchModal({ type: 'openHobby' })}
-            />
-          </Box>
-          <FormControl mb={8}>
-            <FormLabel fontWeight={'bold'}>趣味の紹介</FormLabel>
-            <Textarea
-              placeholder="趣味についての紹介を入力してください"
-              type="text"
-              name="introduceHobby"
-              height="10"
-              value={userInfo.introduceHobby}
-              background="white"
-              onChange={handleChange}
-            />
-          </FormControl>
-        </div>
-      </div>
+              colorScheme="green"
+              isChecked={userInfo.isEmailPublic}
+              value={'public'}
+              onChange={() =>
+                setUserInfo((v) => ({ ...v, isEmailPublic: true }))
+              }>
+              公開
+            </Radio>
+            <Radio
+              bg="white"
+              colorScheme="green"
+              isChecked={!userInfo.isEmailPublic}
+              value={'inPublic'}
+              onChange={() =>
+                setUserInfo((v) => ({ ...v, isEmailPublic: true }))
+              }>
+              非公開
+            </Radio>
+          </Stack>
+        </FormControl>
+        <FormControl className={profileStyles.input_wrapper}>
+          <FormLabel fontWeight={'bold'} fontSize="14px">
+            電話番号
+          </FormLabel>
+          <Input
+            type="phone"
+            name="phone"
+            placeholder="000-0000-0000"
+            background="white"
+            value={userInfo.phone}
+            onChange={handleChange}
+          />
+          <Stack spacing={5} direction="row">
+            <Radio
+              bg="white"
+              colorScheme="green"
+              isChecked={userInfo.isPhonePublic}
+              value={'public'}
+              onChange={() =>
+                setUserInfo((v) => ({ ...v, isPhonePublic: true }))
+              }>
+              公開
+            </Radio>
+            <Radio
+              bg="white"
+              colorScheme="green"
+              isChecked={!userInfo.isPhonePublic}
+              value={'unPublic'}
+              onChange={() =>
+                setUserInfo((v) => ({ ...v, isPhonePublic: false }))
+              }>
+              非公開
+            </Radio>
+          </Stack>
+        </FormControl>
+        <FormControl mb={4}>
+          <FormLabel fontWeight={'bold'} fontSize="14px">
+            所属支社
+          </FormLabel>
+          <Select
+            name="branch"
+            value={userInfo.branch}
+            bg="white"
+            height="10"
+            onChange={handleChange}>
+            <option value={BranchType.NON_SET}>未設定</option>
+            <option value={BranchType.TOKYO}>東京</option>
+            <option value={BranchType.OSAKA}>大阪</option>
+          </Select>
+        </FormControl>
+        <FormControl mb={4}>
+          <FormLabel fontWeight={'bold'} fontSize="14px">
+            自己紹介
+          </FormLabel>
+          <Textarea
+            type="text"
+            name="introduceOther"
+            height="10"
+            placeholder="自己紹介を入力してください"
+            value={userInfo.introduceOther}
+            background="white"
+            onChange={handleChange}
+          />
+        </FormControl>
+        <Box mb={2} w={'100%'}>
+          <FormToLinkTag
+            tags={userInfo?.tags || []}
+            tagType={TagType.TECH}
+            toggleTag={toggleSelectedTag}
+            onEditButtonClick={() => dispatchModal({ type: 'openTech' })}
+          />
+        </Box>
+        <FormControl mb={6}>
+          <FormLabel fontWeight={'bold'} fontSize="14px">
+            技術の紹介
+          </FormLabel>
+          <Textarea
+            placeholder="技術についての紹介を入力してください"
+            type="text"
+            name="introduceTech"
+            height="10"
+            value={userInfo.introduceTech}
+            background="white"
+            onChange={handleChange}
+          />
+        </FormControl>
+        <Box mb={2} w={'100%'}>
+          <FormToLinkTag
+            tags={userInfo?.tags || []}
+            tagType={TagType.QUALIFICATION}
+            toggleTag={toggleSelectedTag}
+            onEditButtonClick={() =>
+              dispatchModal({ type: 'openQualification' })
+            }
+          />
+        </Box>
+        <FormControl mb={6}>
+          <FormLabel fontWeight={'bold'} fontSize="14px">
+            資格の紹介
+          </FormLabel>
+          <Textarea
+            placeholder="資格についての紹介を入力してください"
+            type="text"
+            name="introduceQualification"
+            height="10"
+            value={userInfo.introduceQualification}
+            background="white"
+            onChange={handleChange}
+          />
+        </FormControl>
+        <Box mb={2} w={'100%'}>
+          <FormToLinkTag
+            tags={userInfo?.tags || []}
+            tagType={TagType.CLUB}
+            toggleTag={toggleSelectedTag}
+            onEditButtonClick={() => dispatchModal({ type: 'openClub' })}
+          />
+        </Box>
+        <FormControl mb={6}>
+          <FormLabel fontWeight={'bold'} fontSize="14px">
+            部活動の紹介
+          </FormLabel>
+          <Textarea
+            placeholder="部活動についての紹介を入力してください"
+            type="text"
+            name="introduceClub"
+            height="10"
+            value={userInfo.introduceClub}
+            background="white"
+            onChange={handleChange}
+          />
+        </FormControl>
+        <Box mb={2} w={'100%'}>
+          <FormToLinkTag
+            tags={userInfo?.tags || []}
+            tagType={TagType.HOBBY}
+            toggleTag={toggleSelectedTag}
+            onEditButtonClick={() => dispatchModal({ type: 'openHobby' })}
+          />
+        </Box>
+        <FormControl mb={8}>
+          <FormLabel fontWeight={'bold'} fontSize="14px">
+            趣味の紹介
+          </FormLabel>
+          <Textarea
+            placeholder="趣味についての紹介を入力してください"
+            type="text"
+            name="introduceHobby"
+            height="10"
+            value={userInfo.introduceHobby}
+            background="white"
+            onChange={handleChange}
+          />
+        </FormControl>
+      </Box>
       <Button
         className={profileStyles.update_button_wrapper}
         width="40"
