94036cd3cf164bf03b732da8e3b9c82783eda6cf
takamuu <mr.takamuu@gmail.com>
2022-07-20 13:39:02 +0900

    Remove \'

--- groupware-server/src/migrations/1657888392417-add-event-type.ts
+++ groupware-server/src/migrations/1657888392417-add-event-type.ts
@@ -5,19 +5,19 @@ export class addEventType1657888392417 implements MigrationInterface {
 
   public async up(queryRunner: QueryRunner): Promise<void> {
     await queryRunner.query(
-      `ALTER TABLE \`events\` CHANGE \`type\` \`type\` enum ('impressive_university', 'study_meeting', 'bolday', 'coach', 'club', 'submission_etc', 'other') NOT NULL DEFAULT 'study_meeting'`,
+      `ALTER TABLE events CHANGE type type enum ('impressive_university', 'study_meeting', 'bolday', 'coach', 'club', 'submission_etc', 'other') NOT NULL DEFAULT 'study_meeting'`,
     );
     await queryRunner.query(
-      `ALTER TABLE \`event_introductions\` CHANGE \`type\` \`type\` enum ('impressive_university', 'study_meeting', 'bolday', 'coach', 'club', 'submission_etc', 'other') NOT NULL DEFAULT 'study_meeting'`,
+      `ALTER TABLE event_introductions CHANGE type type enum ('impressive_university', 'study_meeting', 'bolday', 'coach', 'club', 'submission_etc', 'other') NOT NULL DEFAULT 'study_meeting'`,
     );
   }
 
   public async down(queryRunner: QueryRunner): Promise<void> {
     await queryRunner.query(
-      `ALTER TABLE \`event_introductions\` CHANGE \`type\` \`type\` enum ('impressive_university', 'study_meeting', 'bolday', 'coach', 'club', 'submission_etc') NOT NULL DEFAULT 'study_meeting'`,
+      `ALTER TABLE event_introductions CHANGE type type enum ('impressive_university', 'study_meeting', 'bolday', 'coach', 'club', 'submission_etc') NOT NULL DEFAULT 'study_meeting'`,
     );
     await queryRunner.query(
-      `ALTER TABLE \`events\` CHANGE \`type\` \`type\` enum ('impressive_university', 'study_meeting', 'bolday', 'coach', 'club', 'submission_etc') NOT NULL DEFAULT 'study_meeting'`,
+      `ALTER TABLE events CHANGE type type enum ('impressive_university', 'study_meeting', 'bolday', 'coach', 'club', 'submission_etc') NOT NULL DEFAULT 'study_meeting'`,
     );
   }
 }
