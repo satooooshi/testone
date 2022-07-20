cac3740bf29a57f44482acb3ac75b564b91d2c38
shige-lab <shigebask@gmail.com>
2022-07-20 15:54:26 +0900

    refactoring query of user list

--- groupware-server/src/modules/user/user.module.ts
+++ groupware-server/src/modules/user/user.module.ts
@@ -6,12 +6,21 @@ import { ChatModule } from '../chat/chat.module';
 import { StorageModule } from '../storage/storage.module';
 import { UserController } from './user.controller';
 import { UserService } from './user.service';
+import { QAAnswer } from 'src/entities/qaAnswer.entity';
+import { UserJoiningEvent } from 'src/entities/userJoiningEvent.entity';
+import { Wiki } from 'src/entities/wiki.entity';
 
 @Module({
   imports: [
     StorageModule,
     ChatModule,
-    TypeOrmModule.forFeature([User, UserGoodForBoard]),
+    TypeOrmModule.forFeature([
+      User,
+      UserGoodForBoard,
+      UserJoiningEvent,
+      QAAnswer,
+      Wiki,
+    ]),
   ],
   controllers: [UserController],
   providers: [UserService],
