5ad6852f7f01d8e0a487b8fbea87a6b564998a46
k-tkm <kjohlkjou89@gmail.com>
2022-08-02 15:15:54 +0900

    Fixed a bug that caused the same emoji to be displayed in a list in reactions

--- GroupwareMobile/src/components/chat/ChatMessage/index.tsx
+++ GroupwareMobile/src/components/chat/ChatMessage/index.tsx
@@ -24,6 +24,7 @@ import TextMessage from './TextMessage';
 import CallMessage from './CallMessage';
 import VideoMessage from './VideoMessage';
 import StickerMessage from './StickerMessage.tsx';
+import {removeReactionDuplicates} from '../../../utils/removeReactionDuplicate';
 
 type ChatMessageItemProps = {
   message: ChatMessage;
@@ -61,16 +62,6 @@ const ChatMessageItem: React.FC<ChatMessageItemProps> = ({
   const isSender = (emoji: string) => {
     return message?.reactions?.filter(r => r.emoji === emoji && r.isSender)
       .length;
-  };
-  const reactionRemovedDuplicates = (reactions: ChatMessageReaction[]) => {
-    let reactionsNoDuplicates: ChatMessageReaction[] = [];
-    for (const r of reactions) {
-      reactionsNoDuplicates = reactionsNoDuplicates.filter(
-        duplicated => duplicated.emoji !== r.emoji,
-      );
-      reactionsNoDuplicates.push(r);
-    }
-    return reactionsNoDuplicates;
   };
 
   useEffect(() => {
@@ -191,7 +182,7 @@ const ChatMessageItem: React.FC<ChatMessageItemProps> = ({
         flexWrap="wrap"
         alignSelf={message?.isSender ? 'flex-end' : 'flex-start'}>
         {message.reactions?.length
-          ? reactionRemovedDuplicates(message.reactions).map(r => (
+          ? removeReactionDuplicates(message.reactions).map(r => (
               <Div mr="xs" mb="xs" key={r.id}>
                 <ReactionToMessage
                   onPress={() => onPressReaction(r)}
