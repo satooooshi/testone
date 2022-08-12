21d98863af3cfd4f1a1be9d4dcdd6c5cc19e2945
shige-lab <shigebask@gmail.com>
2022-08-12 14:14:59 +0900

    prevent server from being down when a user logout on frontend

--- groupware-frontend/src/hooks/api/auth/useAPILogout.ts
+++ groupware-frontend/src/hooks/api/auth/useAPILogout.ts
@@ -1,6 +1,8 @@
 import { useMutation, UseMutationOptions } from 'react-query';
 import { axiosInstance } from 'src/utils/url';
 import { logoutURL } from 'src/utils/url/auth.url';
+import { jsonHeader } from 'src/utils/url/header';
+import router from 'next/router';
 
 const logout = async () => {
   const res = await axiosInstance.post(logoutURL);
@@ -9,4 +11,19 @@ const logout = async () => {
 
 export const useAPILogout = (mutationOptions?: UseMutationOptions) => {
   return useMutation(logout, mutationOptions);
+};
+
+export const useLogout = () => {
+  const { mutate: logout } = useAPILogout({
+    onSuccess: () => {
+      const removeLocalStorage = async () => {
+        await Promise.resolve();
+        localStorage.removeItem('userToken');
+        axiosInstance.defaults.headers = jsonHeader;
+      };
+      removeLocalStorage();
+      router.push('/login');
+    },
+  });
+  return { logout };
 };
