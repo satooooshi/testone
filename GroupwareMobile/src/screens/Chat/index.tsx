695020da4548937f4cca3f027f7792dc44633fd7
shige-lab <shigebask@gmail.com>
2022-08-28 22:01:14 +0900

    small UI fix

--- GroupwareMobile/src/screens/Chat/index.tsx
+++ GroupwareMobile/src/screens/Chat/index.tsx
@@ -1208,7 +1208,7 @@ const Chat: React.FC = () => {
   return (
     <WholeContainer>
       {typeDropdown}
-      <Div h="100%" bg="blue300">
+      <Div h="100%" bg={Platform.OS === 'ios' ? 'blue300' : 'blue200'}>
         <ReactionsModal
           isVisible={!!selectedReactions}
           selectedReactions={selectedReactions}
