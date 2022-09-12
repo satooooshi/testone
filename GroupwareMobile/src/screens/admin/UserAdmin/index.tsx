1ac09f8b843f7a4d537a3153a82c31927f384c07
satooooshi <saikawa3456@gmail.com>
2022-09-13 00:17:22 +0900

    add top tab screen for admin page

--- GroupwareMobile/src/screens/admin/UserAdmin/index.tsx
+++ GroupwareMobile/src/screens/admin/UserAdmin/index.tsx
@@ -1,3 +1,4 @@
+import {createMaterialTopTabNavigator} from '@react-navigation/material-top-tabs';
 import {useFocusEffect} from '@react-navigation/native';
 import React, {useCallback, useEffect, useState} from 'react';
 import {FlatList} from 'react-native';
@@ -10,15 +11,19 @@ import HeaderWithTextButton from '../../../components/Header';
 import WholeContainer from '../../../components/WholeContainer';
 import {useAdminHeaderTab} from '../../../contexts/admin/useAdminHeaderTab';
 import {useAPIGetUserTag} from '../../../hooks/api/tag/useAPIGetUserTag';
+import {User, UserRole, UserTag} from '../../../types';
+import {userRoleNameFactory} from '../../../utils/factory/userRoleNameFactory';
 import {
   SearchQueryToGetUsers,
   useAPISearchUsers,
 } from '../../../hooks/api/user/useAPISearchUsers';
-import {User, UserTag} from '../../../types';
+
+const TopTab = createMaterialTopTabNavigator();
 
 const UserAdmin: React.FC = () => {
   const [searchQuery, setSearchQuery] = useState<SearchQueryToGetUsers>({
     page: '1',
+    //role: UserRole.COACH,
   });
   const [selectedTags, setSelectedTags] = useState<UserTag[]>([]);
   const {
@@ -77,6 +82,14 @@ const UserAdmin: React.FC = () => {
     }
   }, [searchQuery.tag, tags]);
 
+  const topTabNames = [
+    'AllRole',
+    UserRole.ADMIN,
+    UserRole.COMMON,
+    UserRole.COACH,
+    UserRole.INTERNAL_INSTRUCTOR,
+    UserRole.EXTERNAL_INSTRUCTOR,
+  ];
   return (
     <WholeContainer>
       <HeaderWithTextButton
@@ -99,20 +112,119 @@ const UserAdmin: React.FC = () => {
         right={10}
         onPress={() => setVisibleSearchFormModal(true)}
       />
-      <Div px={16}>
-        {usersForInfiniteScroll.length ? (
-          <FlatList
-            data={usersForInfiniteScroll}
-            {...{onEndReached}}
-            onEndReachedThreshold={0.5}
-            keyExtractor={item => item.id.toString()}
-            ListHeaderComponent={<Div h={10} />}
-            renderItem={({item}) => <UserRow user={item} />}
-          />
-        ) : (
-          <Text fontSize={16}>検索結果が見つかりませんでした</Text>
-        )}
-      </Div>
+      <TopTab.Navigator
+        initialRouteName={topTabNames[0]}
+        screenOptions={{tabBarScrollEnabled: true}}>
+        <TopTab.Screen
+          name={topTabNames[0]}
+          children={() => (
+            <Div px={16}>
+              {usersForInfiniteScroll.length ? (
+                <FlatList
+                  data={usersForInfiniteScroll}
+                  {...{onEndReached}}
+                  onEndReachedThreshold={0.5}
+                  keyExtractor={item => item.id.toString()}
+                  ListHeaderComponent={<Div h={10} />}
+                  renderItem={({item}) => <UserRow user={item} />}
+                />
+              ) : (
+                <Text fontSize={16}>検索結果が見つかりませんでした</Text>
+              )}
+            </Div>
+          )}
+          options={{title: '全て'}}
+        />
+        <TopTab.Screen
+          name={topTabNames[1]}
+          children={() => {
+            const users = usersForInfiniteScroll.filter(
+              u => u.role === UserRole.ADMIN,
+            );
+            return (
+              <Div px={16}>
+                {users.length ? (
+                  <FlatList
+                    data={users}
+                    {...{onEndReached}}
+                    onEndReachedThreshold={0.5}
+                    keyExtractor={item => item.id.toString()}
+                    ListHeaderComponent={<Div h={10} />}
+                    renderItem={({item}) => <UserRow user={item} />}
+                  />
+                ) : (
+                  <Text fontSize={16}>検索結果が見つかりませんでした</Text>
+                )}
+              </Div>
+            );
+          }}
+          options={{title: userRoleNameFactory(UserRole.ADMIN)}}
+        />
+        <TopTab.Screen
+          name={topTabNames[2]}
+          children={() => (
+            <FlatList
+              data={usersForInfiniteScroll.filter(
+                u => u.role === UserRole.COMMON,
+              )}
+              {...{onEndReached}}
+              onEndReachedThreshold={0.5}
+              keyExtractor={item => item.id.toString()}
+              ListHeaderComponent={<Div h={10} />}
+              renderItem={({item}) => <UserRow user={item} />}
+            />
+          )}
+          options={{title: userRoleNameFactory(UserRole.COMMON)}}
+        />
+        <TopTab.Screen
+          name={topTabNames[3]}
+          children={() => (
+            <FlatList
+              data={usersForInfiniteScroll.filter(
+                u => u.role === UserRole.COACH,
+              )}
+              {...{onEndReached}}
+              onEndReachedThreshold={0.5}
+              keyExtractor={item => item.id.toString()}
+              ListHeaderComponent={<Div h={10} />}
+              renderItem={({item}) => <UserRow user={item} />}
+            />
+          )}
+          options={{title: userRoleNameFactory(UserRole.COACH)}}
+        />
+        <TopTab.Screen
+          name={topTabNames[4]}
+          children={() => (
+            <FlatList
+              data={usersForInfiniteScroll.filter(
+                u => u.role === UserRole.INTERNAL_INSTRUCTOR,
+              )}
+              {...{onEndReached}}
+              onEndReachedThreshold={0.5}
+              keyExtractor={item => item.id.toString()}
+              ListHeaderComponent={<Div h={10} />}
+              renderItem={({item}) => <UserRow user={item} />}
+            />
+          )}
+          options={{title: userRoleNameFactory(UserRole.INTERNAL_INSTRUCTOR)}}
+        />
+        <TopTab.Screen
+          name={topTabNames[5]}
+          children={() => (
+            <FlatList
+              data={usersForInfiniteScroll.filter(
+                u => u.role === UserRole.EXTERNAL_INSTRUCTOR,
+              )}
+              {...{onEndReached}}
+              onEndReachedThreshold={0.5}
+              keyExtractor={item => item.id.toString()}
+              ListHeaderComponent={<Div h={10} />}
+              renderItem={({item}) => <UserRow user={item} />}
+            />
+          )}
+          options={{title: userRoleNameFactory(UserRole.EXTERNAL_INSTRUCTOR)}}
+        />
+      </TopTab.Navigator>
       {isLoading && <ActivityIndicator />}
     </WholeContainer>
   );
