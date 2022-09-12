a7b4ac7d9e8cddaf678a1bfaaddc009690cd7b00
osorosia <64416398+osorosia@users.noreply.github.com>
2022-09-12 23:02:34 +0900

    Don't show Pin icon when pin is disabled

--- GroupwareMobile/src/components/chat/RoomCard/index.tsx
+++ GroupwareMobile/src/components/chat/RoomCard/index.tsx
@@ -115,17 +115,19 @@ const RoomCard: React.FC<RoomCardProps> = ({
               }
               style={roomCardStyles.image}
             />
-            <Icon
-              name={room.isPinned ? 'pin' : 'pin-outline'}
-              fontSize={22}
-              color="blue600"
-              p={2}
-              bg="blue100"
-              rounded="circle"
-              fontFamily="MaterialCommunityIcons"
-              style={Platform.OS === 'android' && roomCardStyles.pinIcon}
-              position="absolute"
-            />
+            {room.isPinned ? (
+              <Icon
+                name={room.isPinned ? 'pin' : 'pin-outline'}
+                fontSize={20}
+                color="blue600"
+                p={2}
+                bg="blue100"
+                rounded="circle"
+                fontFamily="MaterialCommunityIcons"
+                style={Platform.OS === 'android' && roomCardStyles.pinIcon}
+                position="absolute"
+              />
+            ) : null}
           </Div>
           <Div flex={1}>
             <Div
