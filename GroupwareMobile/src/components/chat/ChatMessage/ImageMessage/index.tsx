b374dd50ab63b2df9afc6d66db5a7bc61c07cb6d
satooooshi <saikawa3456@gmail.com>
2022-09-15 16:34:59 +0900

    show image in correct aspect ratio in chat page (mobile)

--- GroupwareMobile/src/components/chat/ChatMessage/ImageMessage/index.tsx
+++ GroupwareMobile/src/components/chat/ChatMessage/ImageMessage/index.tsx
@@ -24,7 +24,13 @@ const ImageMessage: React.FC<ImageMessageProps> = ({
             uri: message.content,
             priority: FastImage.priority.low,
           }}
-          style={{height: 144, width: windowWidth * 0.6, borderRadius: 8}}
+          style={{
+            height: 144,
+            width: windowWidth * 0.6,
+            borderRadius: 8,
+            //backgroundColor: '#2a4365',
+          }}
+          resizeMode={FastImage.resizeMode.contain}
         />
       </TouchableOpacity>
     </>
