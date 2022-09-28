3d52b4c6a31362358e519efaf84bb77be3f0be02
shige-lab <shigebask@gmail.com>
2022-09-28 11:13:50 +0900

    tiny fix

--- groupware-frontend/src/components/chat/ChatGroupCard/index.tsx
+++ groupware-frontend/src/components/chat/ChatGroupCard/index.tsx
@@ -127,7 +127,7 @@ const ChatGroupCard: React.FC<ChatGroupCardProps> = ({
           </Text>
         </Box>
         <Text color={darkFontColor} fontSize="14px">
-          {`${chatGroup.members?.length || 0}人のメンバー`}
+          {`${chatGroup.memberCount}人のメンバー`}
         </Text>
         <Box
           display="flex"
