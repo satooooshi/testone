ab4c7f22ecd2e490af488b75458d6ba15811ae04
k-tkm <kjohlkjou89@gmail.com>
2022-10-13 19:00:01 +0900

    Fix to retrive owner when get room detail

--- groupware-server/src/modules/chat/chat.service.ts
+++ groupware-server/src/modules/chat/chat.service.ts
@@ -1218,10 +1218,15 @@ export class ChatService {
       'select users.id as id, users.last_name as lastName, users.first_name as firstName, users.avatar_url as avatarUrl, users.existence as existence from user_chat_leaving INNER JOIN users ON users.existence is not null AND users.id = user_id AND chat_group_id = ?',
       [roomId],
     );
+    const owners: User[] = await manager.query(
+      'select users.id as id, users.last_name as lastName, users.first_name as firstName, users.avatar_url as avatarUrl, users.existence as existence from user_chatGroupOwner_joining INNER JOIN users ON users.existence is not null AND users.id = user_id AND chat_group_id = ?',
+      [roomId],
+    );
 
     // existRoom.previousMembers = previousMembers;
     checkAloneRoom(existRoom, userId);
     existRoom.members = members;
+    existRoom.owner = owners;
     // existRoom.imageURL = await genSignedURL(existRoom.imageURL);
     // existRoom.members = await Promise.all(
     //   members.map(async (m) => ({
