39e94808d308da882770fd79fe0db1ea6e63e086
osorosia <64416398+osorosia@users.noreply.github.com>
2022-10-21 19:36:38 +0900

    fix: SCSS -> Chakra UI

--- GroupwareMobile/src/screens/account/AccountDetail/index.tsx
+++ GroupwareMobile/src/screens/account/AccountDetail/index.tsx
@@ -23,7 +23,6 @@ import {useAPIGetUserInfoById} from '../../../hooks/api/user/useAPIGetUserInfoBy
 import {useAPIGetWikiList} from '../../../hooks/api/wiki/useAPIGetWikiList';
 import {useAPIGetUserGoodList} from '../../../hooks/api/wiki/useAPIGetUserGoodList';
 import {useTagType} from '../../../hooks/tag/useTagType';
-import {accountDetailStyles} from '../../../styles/screen/account/accountDetail.style';
 import {
   BoardCategory,
   RoomType,
@@ -287,9 +286,7 @@ const AccountDetail: React.FC = () => {
         }
       />
       {/* <Button onPress={() => SoundPlayer.resume()}>test </Button> */}
-      <ScrollDiv
-        contentContainerStyle={accountDetailStyles.scrollView}
-        bg="white">
+      <ScrollDiv bg="white">
         {profile && (
           <>
             <Div px={18}>
