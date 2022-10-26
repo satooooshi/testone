e7c6f47dd6d68879fa4ac859e09f3fbea3517073
shige-lab <shigebask@gmail.com>
2022-10-26 14:25:29 +0900

    tiny fix

--- groupware-frontend/src/pages/chat/[id].tsx
+++ groupware-frontend/src/pages/chat/[id].tsx
@@ -77,6 +77,7 @@ const ChatDetail = () => {
           type: 'editOwnersModalVisible',
           value: true,
         });
+        break;
       case 'editGroup':
         dispatchModal({
           type: 'editChatGroupModalVisible',
